47469c27e3671ffde406ed89500643ab5bb7a1c1
JerreS <washiwashi1337@gmail.com>
2017-02-22 12:54:42 +0100

    Added missing resource files

---
+++ Tests/Native/AsmResolver.Tests.Native/Resources/RegOrMem8_Reg8_sib.asm
@@ -0,0 +1,97 @@
+use32
+
+add byte [eax+eax], al
+add byte [eax+ecx], al
+add byte [eax+edx], al
+add byte [eax+ebx], al
+add byte [eax+ebp], al
+add byte [eax+esi], al
+add byte [eax+edi], al
+
+add byte [eax+eax*2], al
+add byte [eax+ecx*2], al
+add byte [eax+edx*2], al
+add byte [eax+ebx*2], al
+add byte [eax+ebp*2], al
+add byte [eax+esi*2], al
+add byte [eax+edi*2], al
+
+add byte [eax+eax*4], al
+add byte [eax+ecx*4], al
+add byte [eax+edx*4], al
+add byte [eax+ebx*4], al
+add byte [eax+ebp*4], al
+add byte [eax+esi*4], al
+add byte [eax+edi*4], al
+
+add byte [eax+eax*8], al
+add byte [eax+ecx*8], al
+add byte [eax+edx*8], al
+add byte [eax+ebx*8], al
+add byte [eax+ebp*8], al
+add byte [eax+esi*8], al
+add byte [eax+edi*8], al
+
+add byte [eax+eax+0x1], al
+add byte [eax+ecx+0x2], al
+add byte [eax+edx+0x3], al
+add byte [eax+ebx+0x4], al
+add byte [eax+ebp+0x6], al
+add byte [eax+esi+0x7], al
+add byte [eax+edi+0x8], al
+
+add byte [eax+eax*2+0x1], al
+add byte [eax+ecx*2+0x2], al
+add byte [eax+edx*2+0x3], al
+add byte [eax+ebx*2+0x4], al
+add byte [eax+ebp*2+0x6], al
+add byte [eax+esi*2+0x7], al
+add byte [eax+edi*2+0x8], al
+
+add byte [eax+eax*4+0x1], al
+add byte [eax+ecx*4+0x2], al
+add byte [eax+edx*4+0x3], al
+add byte [eax+ebx*4+0x4], al
+add byte [eax+ebp*4+0x6], al
+add byte [eax+esi*4+0x7], al
+add byte [eax+edi*4+0x8], al
+
+add byte [eax+eax*8+0x1], al
+add byte [eax+ecx*8+0x2], al
+add byte [eax+edx*8+0x3], al
+add byte [eax+ebx*8+0x4], al
+add byte [eax+ebp*8+0x6], al
+add byte [eax+esi*8+0x7], al
+add byte [eax+edi*8+0x8], al
+
+add byte [eax+eax+0x1337], al
+add byte [eax+ecx+0x1337], al
+add byte [eax+edx+0x1337], al
+add byte [eax+ebx+0x1337], al
+add byte [eax+ebp+0x1337], al
+add byte [eax+esi+0x1337], al
+add byte [eax+edi+0x1337], al
+
+add byte [eax+eax*2+0x1337], al
+add byte [eax+ecx*2+0x1337], al
+add byte [eax+edx*2+0x1337], al
+add byte [eax+ebx*2+0x1337], al
+add byte [eax+ebp*2+0x1337], al
+add byte [eax+esi*2+0x1337], al
+add byte [eax+edi*2+0x1337], al
+
+add byte [eax+eax*4+0x1337], al
+add byte [eax+ecx*4+0x1337], al
+add byte [eax+edx*4+0x1337], al
+add byte [eax+ebx*4+0x1337], al
+add byte [eax+ebp*4+0x1337], al
+add byte [eax+esi*4+0x1337], al
+add byte [eax+edi*4+0x1337], al
+
+add byte [eax+eax*8+0x1337], al
+add byte [eax+ecx*8+0x1337], al
+add byte [eax+edx*8+0x1337], al
+add byte [eax+ebx*8+0x1337], al
+add byte [eax+ebp*8+0x1337], al
+add byte [eax+esi*8+0x1337], al
+add byte [eax+edi*8+0x1337], al
