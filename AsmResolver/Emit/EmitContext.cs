a49ef65cfb2374456fa0fa6807fe30c17f3e4b6e
Washi <washiwashi1337@gmail.com>
2018-03-09 00:10:06 +0100

    Rudimentary ImageSection, imports and resources dir buffers.

---
+++ AsmResolver/Emit/EmitContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace AsmResolver.Emit
+{
+    public class EmitContext
+    {
+        public EmitContext(WindowsAssembly assembly)
+        {
+            if (assembly == null)
+                throw new ArgumentNullException("assembly");
+            Assembly = assembly;
+        }
+
+        public WindowsAssembly Assembly
+        {
+            get;
+            private set;
+        }
+    }
+}
