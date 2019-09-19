00b5f509bfa13918c8856f4195d149c56d19ad7f
Washi <washi1337@protonmail.com>
2019-09-19 19:11:54 +0200

    PEFile, DosHeader

---
+++ src/AsmResolver.PE.File/DosHeader.cs
@@ -0,0 +1,114 @@
+using System;
+
+namespace AsmResolver.PE.File
+{
+    /// <summary>
+    /// Represents the DOS header (also known as the MZ header) in the portable executable (PE) file format.
+    /// </summary>
+    public class DosHeader : ISegment
+    {
+        public const ushort ValidPEMagic = 0x5A4D;
+        public const int MinimalDosHeaderLength = 0x40;
+        public const int NewHeaderFieldOffset = 0x3C;
+        public const int DefaultNewHeaderOffset = 0x80;
+
+        private static readonly byte[] DefaultDosHeader = {
+            0x4D, 0x5A, 0x90, 0x00, 0x03, 0x00, 0x00, 0x00, 0x04, 0x00, 0x00, 0x00,
+            0xFF, 0xFF, 0x00, 0x00, 0xB8, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x40, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x80, 0x00, 0x00, 0x00, 
+            0x0E, 0x1F, 0xBA, 0x0E, 0x00, 0xB4, 0x09, 0xCD,0x21, 0xB8, 0x01, 0x4C, 
+            0xCD, 0x21, 0x54, 0x68, 0x69, 0x73, 0x20, 0x70, 0x72, 0x6F, 0x67, 0x72, 
+            0x61, 0x6D, 0x20, 0x63, 0x61, 0x6E, 0x6E, 0x6F, 0x74, 0x20, 0x62, 0x65, 
+            0x20, 0x72, 0x75, 0x6E, 0x20, 0x69, 0x6E, 0x20, 0x44, 0x4F, 0x53, 0x20, 
+            0x6D, 0x6F, 0x64, 0x65, 0x2E, 0x0D, 0x0D, 0x0A, 0x24, 0x00, 0x00, 0x00, 
+            0x00, 0x00, 0x00, 0x00
+        };
+        
+        /// <summary>
+        /// Reads a DOS header structure at the current position of the provided input stream.
+        /// </summary>
+        /// <param name="reader">The input stream to read from.</param>
+        /// <returns>The read DOS header.</returns>
+        /// <exception cref="BadImageFormatException">Occurs when the input stream does not point to a valid DOS header.</exception>
+        public static DosHeader FromReader(IBinaryStreamReader reader)
+        {
+            var header = new byte[MinimalDosHeaderLength];
+            if (reader.ReadBytes(header, 0, header.Length) != header.Length)
+                throw new BadImageFormatException();
+            return new DosHeader(header);
+        }
+
+        private readonly byte[] _stub;
+        
+        /// <summary>
+        /// Creates a new DOS header with the default contents.
+        /// </summary>
+        public DosHeader()
+            : this(DefaultDosHeader)
+        {
+        }
+
+        /// <summary>
+        /// Creates a new DOS header with the provided contents.
+        /// </summary>
+        /// <param name="stub">The raw contents of the header.</param>
+        /// <exception cref="BadImageFormatException">Occurs when the input data does not contain a valid DOS header.</exception>
+        public DosHeader(byte[] stub)
+        {
+            _stub = stub ?? throw new ArgumentNullException(nameof(stub));
+
+            var reader = new ByteArrayReader(stub);
+
+            ushort magic = reader.ReadUInt16();
+            if (magic != ValidPEMagic)
+                throw new BadImageFormatException();
+
+            reader.FileOffset += NewHeaderFieldOffset - 2;
+            NextHeaderOffset = reader.ReadUInt32();
+        }
+
+        /// <summary>
+        /// Gets or sets the offset to the next header (NT header).
+        /// </summary>
+        public uint NextHeaderOffset
+        {
+            get;
+            set;
+        }
+
+        /// <inheritdoc />
+        uint ISegment.FileOffset => 0;
+
+        /// <inheritdoc />
+        uint ISegment.Rva => 0;
+
+        /// <inheritdoc />
+        void ISegment.UpdateOffsets(uint newFileOffset, uint newRva)
+        {
+        }
+
+        /// <inheritdoc />
+        public uint GetPhysicalSize()
+        {
+            return (uint) _stub.Length;
+        }
+
+        /// <inheritdoc />
+        public uint GetVirtualSize()
+        {
+            return (uint) _stub.Length;
+        }
+
+        /// <inheritdoc />
+        public void Write(IBinaryStreamWriter writer)
+        {
+            writer.WriteBytes(_stub, 0, NewHeaderFieldOffset);
+            writer.WriteUInt32(NextHeaderOffset);
+            writer.WriteBytes(_stub, NewHeaderFieldOffset + 4, _stub.Length - NewHeaderFieldOffset - 4);
+        }
+        
+    }
+}
---
+++ src/AsmResolver.PE.File/PEFile.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace AsmResolver.PE.File
+{
+    /// <summary>
+    /// Models a file using the portable executable (PE) file format. It provides access to various PE headers, as well
+    /// as the raw contents of each section present in the file. 
+    /// </summary>
+    public class PEFile
+    {
+        /// <summary>
+        /// Reads a PE file from the disk.
+        /// </summary>
+        /// <param name="path">The file path to the PE file.</param>
+        /// <returns>The PE file that was read.</returns>
+        /// <exception cref="BadImageFormatException">Occurs when the file does not follow the PE file format.</exception>
+        public static PEFile FromFile(string path)
+        {
+            return FromReader(new ByteArrayReader(System.IO.File.ReadAllBytes(path)));
+        }
+
+        /// <summary>
+        /// Reads a PE file from memory.
+        /// </summary>
+        /// <param name="raw">The raw bytes representing the contents of the PE file to read.</param>
+        /// <returns>The PE file that was read.</returns>
+        /// <exception cref="BadImageFormatException">Occurs when the file does not follow the PE file format.</exception>
+        public static PEFile FromBytes(byte[] raw)
+        {
+            return FromReader(new ByteArrayReader(raw));
+        }
+        
+        /// <summary>
+        /// Reads a PE file from the provided input stream.
+        /// </summary>
+        /// <param name="reader">The input stream to read from.</param>
+        /// <returns>The PE file that was read.</returns>
+        /// <exception cref="BadImageFormatException">Occurs when the file does not follow the PE file format.</exception>
+        public static PEFile FromReader(IBinaryStreamReader reader)
+        {
+            var file = new PEFile
+            {
+                DosHeader = DosHeader.FromReader(reader),
+            };
+
+            return file;
+        }
+
+        private PEFile()
+        {
+        }
+
+        /// <summary>
+        /// Gets the DOS header of the PE file.
+        /// </summary>
+        public DosHeader DosHeader
+        {
+            get;
+            set;
+        }
+        
+    }
+}
