9d960085654c20cb9af73d329f133141f3c0b2e6
Washi <washi1337@protonmail.com>
2019-10-28 21:03:19 +0100

    Read/write support for tiny method body format

---
+++ src/AsmResolver.PE/DotNet/Cil/CilRawMethodBody.cs
@@ -0,0 +1,99 @@
+// AsmResolver - Executable file format inspection library 
+// Copyright (C) 2016-2019 Washi
+// 
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 3.0 of the License, or (at your option) any later version.
+// 
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
+using System;
+
+namespace AsmResolver.PE.DotNet.Cil
+{
+    /// <summary>
+    /// When overridden from this class, represents a chunk of CIL code that implements a method body.
+    /// </summary>
+    public abstract class CilRawMethodBody : ISegment
+    {
+        /// <summary>
+        /// Reads a raw method body from the given binary input stream.
+        /// </summary>
+        /// <param name="reader">The binary input stream to read from.</param>
+        /// <returns>The raw method body.</returns>
+        /// <exception cref="NotSupportedException">Occurs when the method header indicates an invalid or unsupported
+        /// method body format.</exception>
+        public static CilRawMethodBody FromReader(IBinaryStreamReader reader)
+        {
+            var flag = (CilMethodBodyAttributes) reader.ReadByte();
+            reader.FileOffset--;
+
+            if ((flag & CilMethodBodyAttributes.Tiny) == CilMethodBodyAttributes.Tiny)
+                return CilRawTinyMethodBody.FromReader(reader);
+            
+            // TODO:
+            // if ((flag & CilMethodBodyAttributes.Fat) == CilMethodBodyAttributes.Fat)
+            //    return CilRawFatMethodBody.FromReader(reader);
+            
+            throw new NotSupportedException("Invalid or unsupported method body format.");
+        }
+
+        private byte[] _code;
+
+        /// <inheritdoc />
+        public uint FileOffset
+        {
+            get;
+            private set;
+        }
+
+        /// <inheritdoc />
+        public uint Rva
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether the method body is using the fat format.
+        /// </summary>
+        public abstract bool IsFat
+        {
+            get;
+        }
+
+        /// <summary>
+        /// Gets or sets the raw bytes that make up the CIL code of the method body. 
+        /// </summary>
+        public byte[] Code
+        {
+            get => _code;
+            set => _code = value ?? throw new ArgumentNullException(nameof(value));
+        }
+
+        /// <inheritdoc />
+        public virtual void UpdateOffsets(uint newFileOffset, uint newRva)
+        {
+            FileOffset = newFileOffset;
+            Rva = newRva;
+        }
+
+        /// <inheritdoc />
+        public abstract uint GetPhysicalSize();
+
+        /// <inheritdoc />
+        public uint GetVirtualSize() => GetPhysicalSize();
+
+        /// <inheritdoc />
+        public abstract void Write(IBinaryStreamWriter writer);
+        
+    }
+}
---
+++ test/AsmResolver.PE.Tests/DotNet/Cil/CilRawMethodBodyTest.cs
@@ -0,0 +1,33 @@
+using AsmResolver.PE.DotNet.Cil;
+using AsmResolver.PE.DotNet.Metadata.Tables;
+using AsmResolver.PE.DotNet.Metadata.Tables.Rows;
+using AsmResolver.PE.File;
+using Xunit;
+
+namespace AsmResolver.PE.Tests.DotNet.Cil
+{
+    public class CilRawMethodBodyTest
+    {
+        [Fact]
+        public void DetectTinyMethodBody()
+        {
+            var peFile = PEFile.FromBytes(Properties.Resources.HelloWorld);
+            var peImage = new SerializedPEImage(peFile);
+            var tablesStream = peImage.DotNetDirectory.Metadata.GetStream<TablesStream>();
+            var methodTable = tablesStream.GetTable<MethodDefinitionRow>();
+
+            var reader = peFile.CreateReaderAtRva(methodTable[0].Rva);
+
+            var methodBody = CilRawMethodBody.FromReader(reader);
+
+            Assert.False(methodBody.IsFat);
+            Assert.Equal(new byte[]
+            {
+                0x72, 0x01, 0x00, 0x00, 0x70, // ldstr "Hello, world!"
+                0x28, 0x0B, 0x00, 0x00, 0x0A, // call void [mscorlib] System.Console::WriteLine(string)
+                0x2A                          // ret
+            }, methodBody.Code);
+            
+        }
+    }
+}
