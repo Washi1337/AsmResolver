d02c3f56581dd5142fe95507982ce0630bef0777
zsr2531 <25555226+zsr2531@users.noreply.github.com>
2020-04-02 00:37:54 +0200

    Add first unit test for sizeof calculation

---
+++ test/AsmResolver.DotNet.Tests/Analysis/SizeCalculatorTest.cs
@@ -0,0 +1,23 @@
+﻿using Xunit;
+
+namespace AsmResolver.DotNet.Tests.Analysis
+{
+    public struct CustomStruct
+    {
+        public int Dummy1;
+
+        public int Dummy2;
+    }
+    
+    public class SizeCalculatorTest
+    {
+        [Fact]
+        public void CustomStruct()
+        {
+            var module = ModuleDefinition.FromFile(typeof(SizeCalculatorTest).Assembly.Location);
+            var custom = (TypeDefinition) module.LookupMember(typeof(CustomStruct).MetadataToken);
+            
+            Assert.Equal(8, custom.CalculateSize());
+        }
+    }
+}
