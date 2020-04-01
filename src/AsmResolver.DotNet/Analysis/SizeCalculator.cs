7df3eda37daede2afcf55fd918bbc6b676899115
zsr2531 <25555226+zsr2531@users.noreply.github.com>
2020-04-01 18:02:44 +0200

    Add reserved parameter for bitness

--- src/AsmResolver.DotNet/Analysis/SizeCalculator.cs
+++ src/AsmResolver.DotNet/Analysis/SizeCalculator.cs
@@ -14,25 +14,27 @@ namespace AsmResolver.DotNet.Analysis
         /// Calculates the size of a <see cref="TypeDefinition"/>
         /// </summary>
         /// <param name="typeDefinition">The <see cref="TypeDefinition"/> to calculate the size of</param>
+        /// <param name="is32bit">Reserved</param>
         /// <returns>The size of <paramref name="typeDefinition"/></returns>
         /// <exception cref="NotSupportedException">It isn't possible to compute the size statically</exception>
-        public static int CalculateSize(TypeDefinition typeDefinition)
+        public static int CalculateSize(TypeDefinition typeDefinition, bool? is32bit = false)
         {
             if (!typeDefinition.IsValueType)
             {
                 throw new NotSupportedException();
             }
 
-            return typeDefinition.Fields.Sum(f => CalculateSize(f.Signature.FieldType));
+            return typeDefinition.Fields.Sum(f => CalculateSize(f.Signature.FieldType, is32bit));
         }
 
         /// <summary>
         /// Calculates the size of a <see cref="TypeSignature"/>
         /// </summary>
         /// <param name="typeSignature">The <see cref="TypeSignature"/> to calculate the size of</param>
+        /// <param name="is32bit">Reserved</param>
         /// <returns>The size of <paramref name="typeSignature"/></returns>
         /// <exception cref="NotSupportedException">It isn't possible to compute the size statically</exception>
-        public static int CalculateSize(TypeSignature typeSignature)
+        public static int CalculateSize(TypeSignature typeSignature, bool? is32bit)
         {
             switch (typeSignature.ElementType)
             {
@@ -66,7 +68,7 @@ namespace AsmResolver.DotNet.Analysis
                 
                 case ElementType.Enum:
                 {
-                    return CalculateSize(typeSignature.GetUnderlyingTypeDefOrRef().ToTypeSignature());
+                    return CalculateSize(typeSignature.GetUnderlyingTypeDefOrRef().ToTypeSignature(), is32bit);
                 }
                 
                 case ElementType.ValueType:
@@ -85,10 +87,11 @@ namespace AsmResolver.DotNet.Analysis
         /// Calculates the size of a <see cref="TypeSpecification"/>
         /// </summary>
         /// <param name="typeSpecification">The <see cref="TypeSpecification"/> to calculate the size of</param>
+        /// <param name="is32bit">Reserved</param>
         /// <returns>The size of <paramref name="typeSpecification"/></returns>
-        public static int CalculateSize(TypeSpecification typeSpecification)
+        public static int CalculateSize(TypeSpecification typeSpecification, bool? is32Bit)
         {
-            return CalculateSize(typeSpecification.Signature);
+            return CalculateSize(typeSpecification.Signature, is32Bit);
         }
     }
 }
