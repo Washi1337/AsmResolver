82322d1edf61fff69f0779d4e957fe4c02d9cb7d
zsr2531 <25555226+zsr2531@users.noreply.github.com>
2020-04-01 23:17:02 +0200

    Add new exception for calculation errors

---
+++ src/AsmResolver.DotNet/Analysis/SizeCalculationException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AsmResolver.DotNet.Analysis
+{
+    /// <summary>
+    /// Gets thrown when a calculation error occurs in <see cref="SizeCalculator"/>
+    /// </summary>
+    public sealed class SizeCalculationException : Exception
+    {
+        internal SizeCalculationException(string message)
+            : base(message) { }
+    }
+}
