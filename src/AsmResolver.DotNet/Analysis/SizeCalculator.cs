9b32f12b879ac576ef7befb721bded0910ae3501
zsr2531 <25555226+zsr2531@users.noreply.github.com>
2020-04-02 00:05:48 +0200

    Complete Sizeof calculator

--- src/AsmResolver.DotNet/Analysis/SizeCalculator.cs
+++ src/AsmResolver.DotNet/Analysis/SizeCalculator.cs
@@ -14,27 +14,32 @@ namespace AsmResolver.DotNet.Analysis
         /// Calculates the size of a <see cref="TypeDefinition"/>
         /// </summary>
         /// <param name="typeDefinition">The <see cref="TypeDefinition"/> to calculate the size of</param>
-        /// <param name="is32bit">Reserved</param>
+        /// <param name="is32Bit">Whether the parent <see cref="ModuleDefinition"/> is 32 bit</param>
         /// <returns>The size of <paramref name="typeDefinition"/></returns>
-        /// <exception cref="NotSupportedException">It isn't possible to compute the size statically</exception>
-        public static int CalculateSize(TypeDefinition typeDefinition, bool? is32bit = false)
+        /// <exception cref="SizeCalculationException">It isn't possible to compute the size statically</exception>
+        public static int CalculateSize(TypeDefinition typeDefinition, bool? is32Bit = null)
         {
             if (!typeDefinition.IsValueType)
             {
-                throw new NotSupportedException();
+                if (is32Bit.HasValue)
+                {
+                    return is32Bit.Value ? 4 : 8;
+                }
+                
+                throw new SizeCalculationException("Cannot calculate the size of a reference type");
             }
 
-            return typeDefinition.Fields.Sum(f => CalculateSize(f.Signature.FieldType, is32bit));
+            return typeDefinition.Fields.Sum(f => CalculateSize(f.Signature.FieldType, is32Bit));
         }
 
         /// <summary>
         /// Calculates the size of a <see cref="TypeSignature"/>
         /// </summary>
         /// <param name="typeSignature">The <see cref="TypeSignature"/> to calculate the size of</param>
-        /// <param name="is32bit">Reserved</param>
+        /// <param name="is32Bit">Whether the parent <see cref="ModuleDefinition"/> is 32 bit</param>
         /// <returns>The size of <paramref name="typeSignature"/></returns>
-        /// <exception cref="NotSupportedException">It isn't possible to compute the size statically</exception>
-        public static int CalculateSize(TypeSignature typeSignature, bool? is32bit)
+        /// <exception cref="SizeCalculationException">It isn't possible to compute the size statically</exception>
+        public static int CalculateSize(TypeSignature typeSignature, bool? is32Bit = null)
         {
             switch (typeSignature.ElementType)
             {
@@ -65,20 +70,41 @@ namespace AsmResolver.DotNet.Analysis
                 {
                     return 8;
                 }
+
+                case ElementType.Array:
+                case ElementType.SzArray:
+                case ElementType.Boxed:
+                case ElementType.Class:
+                case ElementType.Object:
+                case ElementType.I:
+                case ElementType.U:
+                case ElementType.String:
+                case ElementType.ByRef:
+                case ElementType.TypedByRef:
+                case ElementType.Ptr:
+                case ElementType.FnPtr:
+                {
+                    if (is32Bit is null)
+                    {
+                        throw new NotSupportedException();
+                    }
+
+                    return is32Bit.Value ? 4 : 8;
+                }
                 
                 case ElementType.Enum:
                 {
-                    return CalculateSize(typeSignature.GetUnderlyingTypeDefOrRef().ToTypeSignature(), is32bit);
+                    return CalculateSize(typeSignature.GetUnderlyingTypeDefOrRef().ToTypeSignature(), is32Bit);
                 }
                 
                 case ElementType.ValueType:
                 {
-                    return CalculateSize(typeSignature.Resolve());
+                    return CalculateSize(typeSignature.Resolve(), is32Bit);
                 }
-                
+
                 default:
                 {
-                    throw new NotSupportedException();
+                    throw new SizeCalculationException($"Unsupported type {typeSignature.Name}");
                 }
             }
         }
@@ -87,9 +113,9 @@ namespace AsmResolver.DotNet.Analysis
         /// Calculates the size of a <see cref="TypeSpecification"/>
         /// </summary>
         /// <param name="typeSpecification">The <see cref="TypeSpecification"/> to calculate the size of</param>
-        /// <param name="is32bit">Reserved</param>
+        /// <param name="is32Bit">Whether the parent <see cref="ModuleDefinition"/> is 32 bit</param>
         /// <returns>The size of <paramref name="typeSpecification"/></returns>
-        public static int CalculateSize(TypeSpecification typeSpecification, bool? is32Bit)
+        public static int CalculateSize(TypeSpecification typeSpecification, bool? is32Bit = null)
         {
             return CalculateSize(typeSpecification.Signature, is32Bit);
         }
