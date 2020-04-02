5811b360424eacae60bc4d7b3dddf9a4ca3320b4
zsr2531 <25555226+zsr2531@users.noreply.github.com>
2020-04-02 14:01:59 +0200

    Add more unit tests

--- test/AsmResolver.DotNet.Tests/Analysis/SizeCalculatorTest.cs
+++ test/AsmResolver.DotNet.Tests/Analysis/SizeCalculatorTest.cs
@@ -1,4 +1,5 @@
-﻿using Xunit;
+﻿using System.Runtime.InteropServices;
+using Xunit;
 
 namespace AsmResolver.DotNet.Tests.Analysis
 {
@@ -8,7 +9,33 @@ namespace AsmResolver.DotNet.Tests.Analysis
 
         public int Dummy2;
     }
+
+    public struct CustomGenericStruct<T>
+    {
+        public T Dummy1;
+
+        public ulong Dummy2;
+    }
+
+    [StructLayout(LayoutKind.Sequential, Size = 8)]
+    public struct CustomStructWithBigSize
+    {
+        public int Dummy1;
+    }
     
+    [StructLayout(LayoutKind.Sequential, Size = 2)]
+    public struct CustomStructWithSmallSize
+    {
+        public int Dummy1;
+    }
+
+    [StructLayout(LayoutKind.Explicit)]
+    public struct CustomStructWithFieldOffset
+    {
+        [FieldOffset(2)]
+        public long Dummy1;
+    }
+
     public class SizeCalculatorTest
     {
         [Fact]
@@ -18,6 +45,42 @@ namespace AsmResolver.DotNet.Tests.Analysis
             var custom = (TypeDefinition) module.LookupMember(typeof(CustomStruct).MetadataToken);
             
             Assert.Equal(8, custom.CalculateSize());
+        }
+
+        [Fact]
+        public void CustomGenericStruct()
+        {
+            var module = ModuleDefinition.FromFile(typeof(SizeCalculatorTest).Assembly.Location);
+            var custom = (TypeDefinition) module.LookupMember(typeof(CustomGenericStruct<byte>).MetadataToken);
+            
+            Assert.Equal(9, custom.CalculateSize());
+        }
+
+        [Fact]
+        public void CustomStructWithBigSize()
+        {
+            var module = ModuleDefinition.FromFile(typeof(SizeCalculatorTest).Assembly.Location);
+            var custom = (TypeDefinition) module.LookupMember(typeof(CustomStructWithBigSize).MetadataToken);
+            
+            Assert.Equal(8, custom.CalculateSize());
+        }
+
+        [Fact]
+        public void CustomStructWithSmallSize()
+        {
+            var module = ModuleDefinition.FromFile(typeof(SizeCalculatorTest).Assembly.Location);
+            var custom = (TypeDefinition) module.LookupMember(typeof(CustomStructWithSmallSize).MetadataToken);
+            
+            Assert.Equal(4, custom.CalculateSize());
+        }
+
+        [Fact]
+        public void CustomStructWithFieldOffset()
+        {
+            var module = ModuleDefinition.FromFile(typeof(SizeCalculatorTest).Assembly.Location);
+            var custom = (TypeDefinition) module.LookupMember(typeof(CustomStructWithFieldOffset).MetadataToken);
+            
+            Assert.Equal(10, custom.CalculateSize());
         }
     }
 }
