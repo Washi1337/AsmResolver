fcb6e53fa1f08a02fddcf69ae62eee2ad744069e
zsr2531 <25555226+zsr2531@users.noreply.github.com>
2020-04-02 18:09:41 +0200

    Add calculation strategies

---
+++ src/AsmResolver.DotNet/Analysis/SizeCalculation/AlignEachCalculationStrategy.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+
+namespace AsmResolver.DotNet.Analysis.SizeCalculation
+{
+    /// <summary>
+    /// Aligns every field while summing them up
+    /// </summary>
+    public sealed class AlignEachCalculationStrategy : ISizeCalculationStrategy
+    {
+        /// <summary>
+        /// Creates a new instance of <see cref="AlignEachCalculationStrategy"/> with
+        /// the provided <paramref name="alignment"/>
+        /// </summary>
+        /// <param name="alignment">The alignment to align by</param>
+        public AlignEachCalculationStrategy(int alignment)
+        {
+            _alignment = alignment;
+        }
+
+        private readonly int _alignment;
+        
+        /// <inheritdoc />
+        public int CalculateSize(ITypeDescriptor typeDescriptor, bool? is32Bit)
+        {
+            return typeDescriptor.Resolve().Fields.Select(f =>
+                (int)((uint)f.Signature.FieldType.CalculateSize(is32Bit)).Align((uint)_alignment)
+            ).Sum();
+        }
+    }
+}
---
+++ src/AsmResolver.DotNet/Analysis/SizeCalculation/BiggestSizeCalculationStrategy.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+
+namespace AsmResolver.DotNet.Analysis.SizeCalculation
+{
+    /// <summary>
+    /// Returns the size of the type's biggest field, this is needed for types with an explicit layout
+    /// </summary>
+    public sealed class BiggestSizeCalculationStrategy : ISizeCalculationStrategy
+    {
+        /// <inheritdoc />
+        public int CalculateSize(ITypeDescriptor typeDescriptor, bool? is32Bit)
+        {
+            return typeDescriptor.Resolve().Fields.Select(f =>
+                f.FieldOffset.GetValueOrDefault() + f.Signature.FieldType.CalculateSize(is32Bit)
+            ).Max();
+        }
+    }
+}
