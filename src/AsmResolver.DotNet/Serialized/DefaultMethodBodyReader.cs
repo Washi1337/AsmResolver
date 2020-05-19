1664e3f2d09fb726fe909fe8cb166a275ffcba6e
Washi <washi1337@protonmail.com>
2020-05-19 15:24:05 +0200

    Add DefaultMethodBodyReader.ThrowOnInvalidMethodBody.

--- src/AsmResolver.DotNet/Serialized/DefaultMethodBodyReader.cs
+++ src/AsmResolver.DotNet/Serialized/DefaultMethodBodyReader.cs
@@ -12,24 +12,41 @@ namespace AsmResolver.DotNet.Serialized
     /// </summary>
     public class DefaultMethodBodyReader : IMethodBodyReader
     {
+        /// <summary>
+        /// Gets or sets a value indicating whether invalid method bodies should be ignored and <c>null</c> should be
+        /// returned instead.
+        /// </summary>
+        public bool ThrowOnInvalidMethodBody
+        {
+            get;
+            set;
+        } = true;
+        
         /// <inheritdoc />
         public MethodBody ReadMethodBody(MethodDefinition owner, MethodDefinitionRow row)
         {
-            if (row.Body.CanRead)
+            try
             {
-                if (owner.IsIL)
+                if (row.Body.CanRead)
                 {
-                    var rawBody = CilRawMethodBody.FromReader(row.Body.CreateReader());
-                    return CilMethodBody.FromRawMethodBody(owner, rawBody);
+                    if (owner.IsIL)
+                    {
+                        var rawBody = CilRawMethodBody.FromReader(row.Body.CreateReader());
+                        return CilMethodBody.FromRawMethodBody(owner, rawBody);
+                    }
+                    else
+                    {
+                        // TODO: handle native method bodies.
+                    }
                 }
-                else
+                else if (row.Body.IsBounded && row.Body.GetSegment() is CilRawMethodBody rawMethodBody)
                 {
-                    // TODO: handle native method bodies.
+                    return CilMethodBody.FromRawMethodBody(owner, rawMethodBody);
                 }
             }
-            else if (row.Body.IsBounded && row.Body.GetSegment() is CilRawMethodBody rawMethodBody)
+            catch when (!ThrowOnInvalidMethodBody)
             {
-                return CilMethodBody.FromRawMethodBody(owner, rawMethodBody);
+                return null;
             }
 
             return null;
