6bb128f4867690b11f61fb2b842dad00522e952e
zsr2531 <warhaamer1337@gmail.com>
2020-05-22 22:31:23 +0200

    Flatten value type into list of signatures

--- src/AsmResolver.DotNet/Analysis/TypeMemoryLayoutDetector.cs
+++ src/AsmResolver.DotNet/Analysis/TypeMemoryLayoutDetector.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using System.Collections.Generic;
 using AsmResolver.DotNet.Signatures;
 using AsmResolver.PE.DotNet.Metadata.Tables.Rows;
 
@@ -37,20 +37,43 @@ namespace AsmResolver.DotNet.Analysis
                 ElementType.ByRef => is32Bit ? 4 : 8,
                 ElementType.Class => is32Bit ? 4 : 8,
                 ElementType.Array => is32Bit ? 4 : 8,
-                ElementType.GenericInst => CalculateSize(typeSignature.Resolve().ToTypeSignature(), is32Bit, context.WithType((GenericInstanceTypeSignature) typeSignature)),
-                ElementType.MVar => CalculateSize(context.GetTypeArgument((GenericParameterSignature) typeSignature), is32Bit, context),
-                ElementType.Var => CalculateSize(context.GetTypeArgument((GenericParameterSignature) typeSignature), is32Bit, context),
+                ElementType.GenericInst => CalculateSize(typeSignature.Resolve().ToTypeSignature(), is32Bit,
+                    context.WithType((GenericInstanceTypeSignature) typeSignature)),
+                ElementType.MVar => CalculateSize(
+                    context.GetTypeArgument((GenericParameterSignature) typeSignature), is32Bit, context),
+                ElementType.Var => CalculateSize(
+                    context.GetTypeArgument((GenericParameterSignature) typeSignature), is32Bit, context),
                 ElementType.TypedByRef => is32Bit ? 4 : 8,
                 ElementType.I => is32Bit ? 4 : 8,
                 ElementType.U => is32Bit ? 4 : 8,
                 ElementType.FnPtr => is32Bit ? 4 : 8,
                 ElementType.Object => is32Bit ? 4 : 8,
                 ElementType.SzArray => is32Bit ? 4 : 8,
-                ElementType.CModReqD => CalculateSize(((CustomModifierTypeSignature) typeSignature).BaseType, is32Bit, context),
-                ElementType.CModOpt => CalculateSize(((CustomModifierTypeSignature) typeSignature).BaseType, is32Bit, context),
+                ElementType.CModReqD => CalculateSize(
+                    ((CustomModifierTypeSignature) typeSignature).BaseType, is32Bit, context),
+                ElementType.CModOpt => CalculateSize(
+                    ((CustomModifierTypeSignature) typeSignature).BaseType, is32Bit, context),
                 ElementType.Boxed => is32Bit ? 4 : 8,
                 _ => throw new TypeMemoryLayoutDetectionException()
             };
+        }
+
+        private static IReadOnlyList<TypeSignature> FlattenValueType(
+            TypeDefinition valueType, HashSet<TypeDefinition> visited = null)
+        {
+            var signatures = new List<TypeSignature>();
+            visited ??= new HashSet<TypeDefinition>();
+            visited.Add(valueType);
+
+            foreach (var field in valueType.Fields)
+            {
+                var sig = field.Signature.FieldType;
+                if (sig.ElementType == ElementType.ValueType)
+                    signatures.AddRange(FlattenValueType(sig.Resolve(), visited));
+                else signatures.Add(sig);
+            }
+            
+            return signatures;
         }
     }
 }
