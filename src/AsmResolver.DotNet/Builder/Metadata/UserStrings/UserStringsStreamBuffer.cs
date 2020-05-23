1a9f683ee52c3050707089e0ce86be4bf8e1f276
Washi <washi1337@protonmail.com>
2020-05-23 15:53:33 +0200

    Switch to lower level approach for importing us stream.

--- src/AsmResolver.DotNet/Builder/Metadata/UserStrings/UserStringsStreamBuffer.cs
+++ src/AsmResolver.DotNet/Builder/Metadata/UserStrings/UserStringsStreamBuffer.cs
@@ -47,15 +47,44 @@ namespace AsmResolver.DotNet.Builder.Metadata.UserStrings
         /// <param name="stream">The stream to import.</param>
         public void ImportUserStringsStream(UserStringsStream stream)
         {
+            if (!stream.CanRead)
+                throw new ArgumentException("User strings stream to import must be readable.");
+
+            var reader = stream.CreateReader();
+            
+            // Only copy first byte to output stream if we have already written something to the output stream.   
+            byte b = reader.ReadByte();
+            if (_rawStream.Length != 1)
+                _writer.WriteByte(b);
+
+            // Perform linear sweep of the raw data. 
+            
+            // Note: This might result in incorrect strings being indexed if garbage data was injected in the heap. 
+            //       This is okay as long as we still copy all the data, including the garbage data.
+            //       The only side-effect we get is that strings that did appear in the original stream might 
+            //       be duplicated in the new stream. This is an acceptable side-effect, as the purpose of this
+            //       import function is to only preserve existing data, and not necessarily make sure that we use
+            //       the most efficient storage mechanism.
+            
             uint index = 1;
             while (index < stream.GetPhysicalSize())
             {
-                var @string = stream.GetStringByIndex(index);
-                uint newIndex = AppendString(@string);
-                _strings[@string] = newIndex;
+                if (!reader.TryReadCompressedUInt32(out uint length))
+                    break;
+                
+                // Read string at index.
+                uint newIndex = (uint) _rawStream.Length;
+                _strings[stream.GetStringByIndex(index)] = newIndex;
+                    
+                // Copy over raw data of string to output stream.
+                // This is important since technically it is possible to encode the same string in multiple ways.
+                var buffer = new byte[length];
+                int actualLength = reader.ReadBytes(buffer, 0, (int) length);
+                _writer.WriteCompressedUInt32(length);
+                _writer.WriteBytes(buffer, 0, actualLength);
 
-                uint byteCount = (uint) Encoding.Unicode.GetByteCount(@string);
-                index += byteCount.GetCompressedSize() + byteCount; ;
+                // Move to next user string.
+                index += (uint) _rawStream.Length - newIndex;
             }
         }
 
@@ -77,9 +106,14 @@ namespace AsmResolver.DotNet.Builder.Metadata.UserStrings
 
         private uint AppendString(string value)
         {
-            uint offset;
-            offset = (uint) _rawStream.Length;
+            uint offset = (uint) _rawStream.Length;
 
+            if (string.IsNullOrEmpty(value))
+            {
+                _writer.WriteByte(0);
+                return offset;
+            }
+            
             int byteCount = Encoding.Unicode.GetByteCount(value) + 1;
             _writer.WriteCompressedUInt32((uint) byteCount);
 
