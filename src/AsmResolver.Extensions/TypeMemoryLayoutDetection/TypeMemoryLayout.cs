601dff20d1d9a994e9d3717f88eef4433ca3807b
zsr2531 <warhaamer1337@gmail.com>
2020-05-24 17:16:05 +0200

    Add missing XMLDOC from public members

--- src/AsmResolver.Extensions/TypeMemoryLayoutDetection/TypeMemoryLayout.cs
+++ src/AsmResolver.Extensions/TypeMemoryLayoutDetection/TypeMemoryLayout.cs
@@ -3,6 +3,9 @@ using AsmResolver.DotNet;
 
 namespace AsmResolver.Extensions.TypeMemoryLayoutDetection
 {
+    /// <summary>
+    /// Provides information about how a type is laid out in memory
+    /// </summary>
     public sealed class TypeMemoryLayout
     {
         private readonly IReadOnlyDictionary<FieldDefinition, int> _offsets;
@@ -13,11 +16,19 @@ namespace AsmResolver.Extensions.TypeMemoryLayoutDetection
             Size = size;
         }
 
+        /// <summary>
+        /// The size of the type in memory, in bytes
+        /// </summary>
         public int Size
         {
             get;
         }
 
+        /// <summary>
+        /// Gets the raw offset of the field in memory
+        /// </summary>
+        /// <param name="fieldDefinition">The field to get the offset of</param>
+        /// <returns>The offset of the field, in bytes</returns>
         public int GetFieldOffset(FieldDefinition fieldDefinition)
         {
             return _offsets[fieldDefinition];
