f388872186e03522124e2fb79a9455ea26bd589a
zsr2531 <warhaamer1337@gmail.com>
2020-05-24 17:09:09 +0200

    Add type memory layout result and skeleton detector

---
+++ src/AsmResolver.Extensions/TypeMemoryLayoutDetection/TypeMemoryLayout.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using AsmResolver.DotNet;
+
+namespace AsmResolver.Extensions.TypeMemoryLayoutDetection
+{
+    public sealed class TypeMemoryLayout
+    {
+        private readonly IReadOnlyDictionary<FieldDefinition, int> _offsets;
+
+        internal TypeMemoryLayout(IReadOnlyDictionary<FieldDefinition, int> offsets, int size)
+        {
+            _offsets = offsets;
+            Size = size;
+        }
+
+        public int Size
+        {
+            get;
+        }
+
+        public int GetFieldOffset(FieldDefinition fieldDefinition)
+        {
+            return _offsets[fieldDefinition];
+        }
+    }
+}
---
+++ src/AsmResolver.Extensions/TypeMemoryLayoutDetection/TypeMemoryLayoutDetector.cs
@@ -0,0 +1,23 @@
+﻿using AsmResolver.DotNet;
+using AsmResolver.DotNet.Signatures.Types;
+
+namespace AsmResolver.Extensions.TypeMemoryLayoutDetection
+{
+    public static class TypeMemoryLayoutDetector
+    {
+        public static TypeMemoryLayout GetImpliedMemoryLayout(this TypeDefinition typeDefinition, bool is32Bit)
+        {
+            return GetImpliedMemoryLayout(typeDefinition.ToTypeSignature(), is32Bit);
+        }
+
+        public static TypeMemoryLayout GetImpliedMemoryLayout(this TypeSpecification typeSpecification, bool is32Bit)
+        {
+            return GetImpliedMemoryLayout(typeSpecification.Signature, is32Bit);
+        }
+
+        public static TypeMemoryLayout GetImpliedMemoryLayout(this TypeSignature typeSignature, bool is32Bit)
+        {
+            return new TypeMemoryLayout(null, 0);
+        }
+    }
+}
