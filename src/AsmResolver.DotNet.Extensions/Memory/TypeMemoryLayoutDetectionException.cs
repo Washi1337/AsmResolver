23b23ca10d73328626201d1f5f9a0b5d23b6a26a
zsr2531 <warhaamer1337@gmail.com>
2020-05-26 15:46:05 +0200

    Add an exception that is thrown in the layout detector

---
+++ src/AsmResolver.DotNet.Extensions/Memory/TypeMemoryLayoutDetectionException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AsmResolver.DotNet.Extensions.Memory
+{
+    /// <summary>
+    /// Gets thrown when the layout of a type cannot be inferred
+    /// </summary>
+    public sealed class TypeMemoryLayoutDetectionException : Exception
+    {
+        internal TypeMemoryLayoutDetectionException(string message)
+            : base(message) { }
+    }
+}
