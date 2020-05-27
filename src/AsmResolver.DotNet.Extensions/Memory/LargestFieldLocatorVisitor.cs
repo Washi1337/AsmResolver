6cc0290fec1177f7e796139e19b8708aa0dafad9
zsr2531 <warhaamer1337@gmail.com>
2020-05-27 17:05:11 +0200

    Add visitor to find biggest field (needed for alignment)

---
+++ src/AsmResolver.DotNet.Extensions/Memory/LargestFieldLocatorVisitor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace AsmResolver.DotNet.Extensions.Memory
+{
+    internal sealed class LargestFieldLocatorVisitor : IVisitor
+    {
+        private readonly bool _is32Bit;
+
+        internal LargestFieldLocatorVisitor(bool is32Bit)
+        {
+            _is32Bit = is32Bit;
+        }
+        
+        internal uint Largest
+        {
+            get;
+            private set;
+        }
+        
+        public void VisitComplex(FieldNode node)
+        {
+            foreach (var child in node.Children)
+                child.Accept(this);
+        }
+
+        public void VisitPrimitive(FieldNode node)
+        {
+            Largest = Math.Max(Largest, node.Signature.SizeInBytes(_is32Bit));
+        }
+    }
+}
