6360c261fc688cc10f662f440c7101a2afd82633
zsr2531 <warhaamer1337@gmail.com>
2020-05-27 20:30:57 +0200

    Refactor and add comments in explicit visitor

--- src/AsmResolver.DotNet.Extensions/Memory/ExplicitLayoutVisitor.cs
+++ src/AsmResolver.DotNet.Extensions/Memory/ExplicitLayoutVisitor.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace AsmResolver.DotNet.Extensions.Memory
 {
     internal sealed class ExplicitLayoutVisitor : VisitorBase
     {
         private readonly Dictionary<FieldDefinition, uint> _offsets = new Dictionary<FieldDefinition, uint>();
-        private uint _max;
+        private uint _size;
 
         internal ExplicitLayoutVisitor(TypeDefinition parent, uint alignment, bool is32Bit)
             : base(parent, alignment, is32Bit) { }
@@ -14,25 +15,37 @@ namespace AsmResolver.DotNet.Extensions.Memory
         public override void VisitComplex(FieldNode node)
         {
             var layout = node.Signature.GetImpliedMemoryLayout(Is32Bit);
-            var offset = node.ExplicitOffset ?? 0;
-            _offsets[node.Field] = offset;
-            _max = Math.Max(_max, offset + layout.Size);
+            CommenceInference(node.Field, layout.Size);
         }
 
         public override void VisitPrimitive(FieldNode node)
         {
-            var explicitOffset = node.ExplicitOffset ?? 0;
-
-            _offsets[node.Field] = explicitOffset;
-            _max = Math.Max(_max, explicitOffset + node.Signature.SizeInBytes(Is32Bit));
+            CommenceInference(node.Field, node.Signature.SizeInBytes(Is32Bit));
         }
 
         internal override TypeMemoryLayout ConstructLayout()
         {
-            var realSize = _max.Align(Alignment);
-            var explicitSize = Parent.ClassLayout?.ClassSize ?? 0;
+            // We first need to align the type it to its alignment
+            var inferredSize = _size.Align(Alignment); 
             
-            return new TypeMemoryLayout(_offsets, Math.Max(realSize, explicitSize));
+            // We try to get the explicit size, if there is none, we'll just use 0
+            var explicitSize = Parent.ClassLayout?.ClassSize ?? 0; 
+            
+            // If there is an explicitly set size for the struct, the "real" size is
+            // the bigger one of the explicit size or the inferred size
+            return new TypeMemoryLayout(_offsets, Math.Max(inferredSize, explicitSize));
+        }
+
+        private void CommenceInference(FieldDefinition field, uint inferredSize)
+        {
+            Debug.Assert(field.FieldOffset != null, "A field in an explicit layout struct must have a FieldOffset");
+            var offset = (uint) field.FieldOffset;
+
+            // The offset is simply the explicit offset
+            _offsets[field] = offset;
+            
+            // The size of an explicit struct is its largest field
+            _size = Math.Max(_size, offset + inferredSize);
         }
     }
 }
