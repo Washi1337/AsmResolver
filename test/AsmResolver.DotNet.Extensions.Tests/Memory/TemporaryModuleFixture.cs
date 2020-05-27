f0bb7ab3ab3d2d6721a7158a599479a937db343e
zsr2531 <warhaamer1337@gmail.com>
2020-05-27 22:29:26 +0200

    Add unit test fixture for type mem layout tests

---
+++ test/AsmResolver.DotNet.Extensions.Tests/Memory/TemporaryModuleFixture.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AsmResolver.DotNet.Extensions.Tests.Memory
+{
+    public sealed class TemporaryModuleFixture
+    {
+        public ModuleDefinition Module
+        {
+            get;
+        } = ModuleDefinition.FromFile(typeof(TemporaryModuleFixture).Assembly.Location);
+
+        public TypeDefinition LookupType(Type type)
+        {
+            return (TypeDefinition) Module.LookupMember(type.MetadataToken);
+        }
+    }
+}
