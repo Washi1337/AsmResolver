b1480f13b4128beb58392e75fbb3935500bc7fb9
zsr2531 <warhaamer1337@gmail.com>
2020-05-27 22:57:07 +0200

    Add test for cyclic struct

---
+++ test/AsmResolver.DotNet.Extensions.Tests/Memory/CyclicStructTest.cs
@@ -0,0 +1,39 @@
+﻿using AsmResolver.DotNet.Extensions.Memory;
+using AsmResolver.DotNet.Signatures;
+using AsmResolver.PE.DotNet.Metadata.Tables.Rows;
+using Xunit;
+
+namespace AsmResolver.DotNet.Extensions.Tests.Memory
+{
+    public sealed class CyclicStructTest : IClassFixture<TemporaryModuleFixture>
+    {
+        private readonly TemporaryModuleFixture _fixture;
+
+        public CyclicStructTest(TemporaryModuleFixture fixture)
+        {
+            _fixture = fixture;
+        }
+        
+        struct Normal
+        {
+            // We will place a field of type Abnormal here
+        }
+
+        struct Abnormal
+        {
+            Normal Dummy;
+        }
+        
+        [Fact]
+        public void CyclicStructShouldThrow()
+        {
+            var normal = _fixture.LookupType(typeof(Normal));
+            var abnormal = _fixture.LookupType(typeof(Abnormal));
+
+            var signature = FieldSignature.CreateInstance(abnormal.ToTypeSignature());
+            normal.Fields.Add(new FieldDefinition("Abnormal", FieldAttributes.Private, signature));
+
+            Assert.Throws<TypeMemoryLayoutDetectionException>(() => normal.GetImpliedMemoryLayout(false));
+        }
+    }
+}
