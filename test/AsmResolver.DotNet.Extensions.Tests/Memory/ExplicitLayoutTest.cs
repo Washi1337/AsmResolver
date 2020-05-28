095e9ffc549c90ef0c3f762d72a849a77b770cc3
zsr2531 <warhaamer1337@gmail.com>
2020-05-28 11:51:18 +0200

    Add more unit tests for explicit layout

--- test/AsmResolver.DotNet.Extensions.Tests/Memory/ExplicitLayoutTest.cs
+++ test/AsmResolver.DotNet.Extensions.Tests/Memory/ExplicitLayoutTest.cs
@@ -27,13 +27,51 @@ namespace AsmResolver.DotNet.Extensions.Tests.Memory
             long Dummy2;
         }
 
+        [StructLayout(LayoutKind.Explicit)]
+        struct Struct2
+        {
+            [FieldOffset(0)]
+            int Dummy1;
+
+            [FieldOffset(12)]
+            int Dummy2;
+        }
+
+        [StructLayout(LayoutKind.Explicit)]
+        unsafe struct Struct3
+        {
+            [FieldOffset(0)]
+            double Dummy1;
+
+            [FieldOffset(16)]
+            fixed byte Dummy2[4];
+        }
+
+        [StructLayout(LayoutKind.Explicit)]
+        unsafe struct Struct4
+        {
+            [FieldOffset(0)]
+            int* Dummy1;
+
+            [FieldOffset(8)]
+            ConsoleKey Dummy2;
+        }
+
         [Theory]
-        [InlineData(typeof(Struct1), 8u)]
-        public void NoNest(Type type, uint expected)
+        [InlineData(typeof(Struct1), 8u, new uint[] { 0, 0 })]
+        [InlineData(typeof(Struct2), 16u, new uint[] { 0, 12 })]
+        [InlineData(typeof(Struct3), 24u, new uint[] { 0, 16 })]
+        [InlineData(typeof(Struct4), 16u, new uint[] { 0, 8 })]
+        public void NoNest(Type type, uint expectedSize, uint[] fieldOffsets)
         {
             var target = _fixture.LookupType(type);
-
-            Assert.Equal(expected, target.GetImpliedMemoryLayout(IntPtr.Size == 4).Size);
+            var layout = target.GetImpliedMemoryLayout(IntPtr.Size == 4);
+            
+            Assert.Equal(expectedSize, layout.Size);
+            for (var i = 0; i < target.Fields.Count; i++)
+            {
+                Assert.Equal(fieldOffsets[i], layout.GetFieldOffset(target.Fields[i]));
+            }
         }
     }
 }
