e21b24e84e44a2ca6a4fb705eef0be91df1fc674
zsr2531 <warhaamer1337@gmail.com>
2020-05-28 19:44:34 +0200

    Add extreme unit test case

---
+++ test/AsmResolver.DotNet.Extensions.Tests/Memory/MixedLayoutTest.cs
@@ -0,0 +1,68 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using AsmResolver.DotNet.Extensions.Memory;
+using AsmResolver.DotNet.Signatures.Types;
+using Xunit;
+#pragma warning disable 169
+
+namespace AsmResolver.DotNet.Extensions.Tests.Memory
+{
+    [SuppressMessage("ReSharper", "FieldCanBeMadeReadOnly.Local")]
+    [SuppressMessage("ReSharper", "ArrangeTypeMemberModifiers")]
+    [SuppressMessage("ReSharper", "InconsistentNaming")]
+    public sealed class MixedLayoutTest : IClassFixture<TemporaryModuleFixture>
+    {
+        private readonly TemporaryModuleFixture _fixture;
+        
+        public MixedLayoutTest(TemporaryModuleFixture fixture)
+        {
+            _fixture = fixture;
+        }
+
+        [StructLayout(LayoutKind.Sequential, Size = 17)]
+        struct Struct1
+        {
+            int Dummy1;
+        }
+
+        [StructLayout(LayoutKind.Sequential, Size = 23, Pack = 2)]
+        struct Struct2
+        {
+            Struct1 Nest1;
+        }
+
+        [StructLayout(LayoutKind.Sequential, Size = 87, Pack = 64)]
+        struct Struct3
+        {
+            Struct1 Nest1;
+
+            Struct2 Nest2;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        unsafe struct Struct4
+        {
+            Struct3 Nest1;
+            
+            byte Dummy2;
+        }
+
+        [StructLayout(LayoutKind.Sequential, Pack = 8)]
+        struct Struct5
+        {
+            Struct4 Nest1;
+
+            double Dummy1;
+        }
+
+        [Fact]
+        public void ExtremeNesting()
+        {
+            var target = _fixture.LookupType(typeof(Struct4));
+            var layout = target.GetImpliedMemoryLayout();
+            
+            Assert.Equal((uint) Unsafe.SizeOf<Struct4>(), layout.Size);
+        }
+    }
+}
