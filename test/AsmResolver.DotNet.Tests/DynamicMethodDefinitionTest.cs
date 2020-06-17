998a7fa086ee29d165593df0943ea1bf2ac6e4e6
TobitoFatitoNulled <7544428+TobitoFatitoNulled@users.noreply.github.com>
2020-06-18 01:44:26 +0300

    Unit Tests

--- test/AsmResolver.DotNet.Tests/DynamicMethodDefinitionTest.cs
+++ test/AsmResolver.DotNet.Tests/DynamicMethodDefinitionTest.cs
@@ -1,4 +1,5 @@
 ﻿using System.Linq;
+using System.Reflection;
 using System.Reflection.Emit;
 using AsmResolver.DotNet.Signatures.Types;
 using AsmResolver.DotNet.TestCases.Methods;
@@ -17,6 +18,35 @@ namespace AsmResolver.DotNet.Tests
             DynamicMethod generateDynamicMethod = TDynamicMethod.GenerateDynamicMethod();
 
             var dynamicMethodDefinition = new DynamicMethodDefinition(module, generateDynamicMethod);
+
+            Assert.NotNull(dynamicMethodDefinition);
+
+            Assert.NotEmpty(dynamicMethodDefinition.CilMethodBody.Instructions);
+            
+            Assert.Equal(dynamicMethodDefinition.CilMethodBody.Instructions.Select(q=>q.OpCode),new CilOpCode[]
+            {
+                CilOpCodes.Ldarg_0, 
+                CilOpCodes.Call,
+                CilOpCodes.Ldarg_1,
+                CilOpCodes.Ret
+            });
+            Assert.Equal(dynamicMethodDefinition.Parameters.Select(q=>q.ParameterType),new TypeSignature[]
+            {
+                module.CorLibTypeFactory.String,
+                module.CorLibTypeFactory.Int32
+            });
+        }
+        
+        [Fact]
+        public void RTDynamicMethod()
+        {
+            var module = ModuleDefinition.FromFile(typeof(TDynamicMethod).Assembly.Location);
+
+            DynamicMethod generateDynamicMethod = TDynamicMethod.GenerateDynamicMethod();
+
+            var RTDynamicMethod = generateDynamicMethod.GetType().GetField("m_dynMethod", (BindingFlags) (-1))?.GetValue(generateDynamicMethod); 
+            
+            var dynamicMethodDefinition = new DynamicMethodDefinition(module, RTDynamicMethod);
 
             Assert.NotNull(dynamicMethodDefinition);
 
