43e4204be5719c2babd040c2841e4a2474732811
Washi <washi1337@protonmail.com>
2020-06-30 00:26:08 +0200

    Add more explicit layout structs.

--- test/AsmResolver.DotNet.Extensions.Tests/Memory/ExplicitStructLayoutTest.cs
+++ test/AsmResolver.DotNet.Extensions.Tests/Memory/ExplicitStructLayoutTest.cs
@@ -1,4 +1,3 @@
-using System;
 using Xunit;
 
 namespace AsmResolver.DotNet.Tests.Memory
@@ -14,7 +13,32 @@ namespace AsmResolver.DotNet.Tests.Memory
         public void EmptyStruct() => VerifySize<ExplicitTestStructs.EmptyStruct>();
 
         [Fact]
-        public void SingleFieldStructDefaultPackImplicitSize() => VerifySize<ExplicitTestStructs.SingleFieldStructDefaultPackImplicitSize>();
+        public void SingleFieldStructDefaultPackImplicitSize() => 
+            VerifySize<ExplicitTestStructs.SingleFieldStructDefaultPackImplicitSize>();
+        
+        [Fact]
+        public void MultipleFieldsStructDefaultPackImplicitSize() => 
+            VerifySize<ExplicitTestStructs.MultipleFieldsStructDefaultPackImplicitSize>();
+        
+        [Fact]
+        public void OverlappingFieldsStructDefaultPackImplicitSize() => 
+            VerifySize<ExplicitTestStructs.OverlappingFieldsStructDefaultPackImplicitSize>();
+        
+        [Fact]
+        public void OverlappingFieldsStructDefaultPackExplicitSize() => 
+            VerifySize<ExplicitTestStructs.OverlappingFieldsStructDefaultPackExplicitSize>();
+        
+        [Fact]
+        public void NestedStruct() => 
+            VerifySize<ExplicitTestStructs.NestedStruct>();
+        
+        [Fact]
+        public void NestedStructOverlapping() => 
+            VerifySize<ExplicitTestStructs.NestedStructOverlapping>();
+        
+        [Fact]
+        public void NestedStructInNestedStructOverlapping() => 
+            VerifySize<ExplicitTestStructs.NestedStructInNestedStructOverlapping>();
 
     }
 }
--- test/AsmResolver.DotNet.Extensions.Tests/Memory/ExplicitTestStructs.cs
+++ test/AsmResolver.DotNet.Extensions.Tests/Memory/ExplicitTestStructs.cs
@@ -15,5 +15,62 @@ namespace AsmResolver.DotNet.Tests.Memory
             [FieldOffset(0)]
             public int IntField;
         }
+        
+        [StructLayout(LayoutKind.Explicit)]
+        public struct MultipleFieldsStructDefaultPackImplicitSize
+        {
+            [FieldOffset(0)]
+            public int IntField;
+            [FieldOffset(100)]
+            public long LongField;
+        }
+        
+        [StructLayout(LayoutKind.Explicit)]
+        public struct OverlappingFieldsStructDefaultPackImplicitSize
+        {
+            [FieldOffset(0)]
+            public int IntField;
+            [FieldOffset(0)]
+            public long LongField;
+        }
+        
+        [StructLayout(LayoutKind.Explicit, Size = 100)]
+        public struct OverlappingFieldsStructDefaultPackExplicitSize
+        {
+            [FieldOffset(0)]
+            public int IntField;
+            [FieldOffset(0)]
+            public long LongField;
+        }
+        
+        [StructLayout(LayoutKind.Explicit)]
+        public struct NestedStruct
+        {
+            [FieldOffset(0)]
+            public OverlappingFieldsStructDefaultPackImplicitSize Field1;
+            
+            [FieldOffset(100)]
+            public byte Field2;
+        }
+        
+        [StructLayout(LayoutKind.Explicit)]
+        public struct NestedStructOverlapping
+        {
+            [FieldOffset(0)]
+            public OverlappingFieldsStructDefaultPackImplicitSize Field1;
+            
+            [FieldOffset(0)]
+            public byte Field2;
+        }
+        
+        [StructLayout(LayoutKind.Explicit)]
+        public struct NestedStructInNestedStructOverlapping
+        {
+            [FieldOffset(0)]
+            public NestedStructOverlapping Field1;
+            
+            [FieldOffset(0)]
+            public byte Field2;
+        }
     }
 }
