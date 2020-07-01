0fea36bb76240377676a3e9ba1fb1237d30e2cf4
Washi <washi1337@protonmail.com>
2020-07-01 23:37:54 +0200

    Add TypeMemoryLayout.TryGetFieldPath

--- src/AsmResolver.DotNet/Memory/TypeMemoryLayout.cs
+++ src/AsmResolver.DotNet/Memory/TypeMemoryLayout.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace AsmResolver.DotNet.Memory
 {
@@ -8,7 +9,7 @@ namespace AsmResolver.DotNet.Memory
     /// </summary>
     public class TypeMemoryLayout
     {
-        private readonly IDictionary<FieldDefinition, FieldMemoryLayout> _fields =
+        private readonly Dictionary<FieldDefinition, FieldMemoryLayout> _fields =
             new Dictionary<FieldDefinition, FieldMemoryLayout>();
 
         internal TypeMemoryLayout(ITypeDescriptor type)
@@ -53,5 +54,74 @@ namespace AsmResolver.DotNet.Memory
             get;
             internal set;
         }
+
+        private IEnumerable<FieldMemoryLayout> GetOrderedFields()
+        {
+            return _fields.Values
+                .OrderBy(f => f.Offset)
+                .ThenBy(f => f.ContentsLayout.Size);
+        } 
+
+        /// <summary>
+        /// Finds a field within this type memory layout by its offset.
+        /// </summary>
+        /// <param name="offset">The offset of the field to find.</param>
+        /// <param name="field">When the method returns <c>true</c>, contains the field with the provided offset..</param>
+        /// <returns><c>true</c> if the field with the provided offset existed, <c>false,</c> otherwise.</returns>
+        public bool TryGetFieldAtOffset(uint offset, out FieldMemoryLayout field)
+        {
+            foreach (var entry in GetOrderedFields())
+            {
+                if (entry.Offset != offset)
+                    continue;
+                field = entry;
+                return true;
+            }
+
+            field = null;
+            return false;
+        } 
+
+        /// <summary>
+        /// Traverses the type memory layout tree and finds a field within this type memory layout by its offset.
+        /// </summary>
+        /// <param name="offset">The offset of the field to find.</param>
+        /// <param name="path">The (incomplete) path of fields to traverse to reach the provided field offset.</param>
+        /// <returns><c>true</c> if the offset points to the start of a field, <c>false</c> otherwise.</returns>
+        public bool TryGetFieldPath(uint offset, out IList<FieldMemoryLayout> path)
+        {
+            path = new List<FieldMemoryLayout>();
+
+            var currentLayout = this;
+
+            bool stop = false;
+            while (!stop)
+            {
+                stop = true;
+                
+                foreach (var entry in currentLayout.GetOrderedFields())
+                {
+                    if (offset >= entry.Offset)
+                    {
+                        // We're past any potential candidate.
+                        if (offset >= entry.Offset + entry.ContentsLayout.Size)
+                        {
+                            currentLayout = null;
+                            break;
+                        }
+
+                        // We found the field that contains the offset. Dive into this field.
+                        path.Add(entry);
+                        currentLayout = entry.ContentsLayout;
+                        offset -= entry.Offset;
+                        
+                        stop = false;
+                        break;
+                    }
+                }
+            }
+
+            return path[path.Count - 1].Offset == offset;
+        } 
     }
 }
