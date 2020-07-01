31582289eed1d5529b8e875ee7e77e543a546f12
TheProxyRE <theproxyre@gmail.com>
2020-07-01 18:31:03 +0200

    Add MemberClonerResult original members

--- src/AsmResolver.DotNet/Cloning/MemberCloneResult.cs
+++ src/AsmResolver.DotNet/Cloning/MemberCloneResult.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 
 namespace AsmResolver.DotNet.Cloning
@@ -7,13 +8,17 @@ namespace AsmResolver.DotNet.Cloning
     /// </summary>
     public class MemberCloneResult
     {
+        private readonly IDictionary<IMemberDescriptor, IMemberDescriptor> clonedMembers;
+
         /// <summary>
         /// Creates a new instance of the <see cref="MemberCloneResult"/> class.
         /// </summary>
         /// <param name="clonedMembers">The cloned members.</param>
-        public MemberCloneResult(IEnumerable<IMemberDescriptor> clonedMembers)
+        public MemberCloneResult(IDictionary<IMemberDescriptor, IMemberDescriptor> clonedMembers)
         {
-            ClonedMembers = new List<IMemberDescriptor>(clonedMembers);
+            ClonedMembers = new List<IMemberDescriptor>(this.clonedMembers.Values);
+            OriginalMembers = new List<IMemberDescriptor>(this.clonedMembers.Keys);
+            this.clonedMembers = clonedMembers;
         }
 
         /// <summary>
@@ -22,6 +27,27 @@ namespace AsmResolver.DotNet.Cloning
         public ICollection<IMemberDescriptor> ClonedMembers
         {
             get;
+        }
+
+        /// <summary>
+        /// Gets a collection of all original members.
+        /// </summary>
+        public ICollection<IMemberDescriptor> OriginalMembers
+        {
+            get;
+        }
+
+        /// <summary>
+        /// Gets the cloned <see cref="IMemberDescriptor"/> by its original <see cref="IMemberDescriptor"/>.
+        /// </summary>
+        /// <param name="originalMember">Original <see cref="IMemberDescriptor"/></param>
+        /// <exception cref="ArgumentOutOfRangeException">Occurs when <paramref name="originalMember"/> is not a member of <see cref="OriginalMembers"/></exception>
+        /// <returns>Cloned <see cref="IMemberDescriptor"/></returns>
+        public IMemberDescriptor GetClonedMember(IMemberDescriptor originalMember)
+        {
+            if (clonedMembers.ContainsKey(originalMember))
+                throw new ArgumentOutOfRangeException(nameof(originalMember));
+            return clonedMembers[originalMember];
         }
     }
 }
