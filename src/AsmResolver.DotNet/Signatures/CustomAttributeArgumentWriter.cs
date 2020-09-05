f778a60c0ee28ef6ed7ede9d1afe28f9d6736779
Washi <washi1337@protonmail.com>
2020-09-05 14:08:35 +0200

    Attempt to write enum values based on the type of CustomAttributeArgument.Element

--- src/AsmResolver.DotNet/Signatures/CustomAttributeArgumentWriter.cs
+++ src/AsmResolver.DotNet/Signatures/CustomAttributeArgumentWriter.cs
@@ -55,52 +55,67 @@ namespace AsmResolver.DotNet.Signatures
                 case ElementType.Boolean:
                     writer.WriteByte((byte) ((bool) element ? 1 : 0));
                     break;
+                
                 case ElementType.Char:
                     writer.WriteUInt16((char) element);
                     break;
+                
                 case ElementType.I1:
                     writer.WriteSByte((sbyte) element);
                     break;
+                
                 case ElementType.U1:
                     writer.WriteByte((byte) element);
                     break;
+                
                 case ElementType.I2:
                     writer.WriteInt16((short) element);
                     break;
+                
                 case ElementType.U2:
                     writer.WriteUInt16((ushort) element);
                     break;
                 case ElementType.I4:
                     writer.WriteInt32((int) element);
                     break;
+                
                 case ElementType.U4:
                     writer.WriteUInt32((uint) element);
                     break;
+                
                 case ElementType.I8:
                     writer.WriteInt64((long) element);
                     break;
+                
                 case ElementType.U8:
                     writer.WriteUInt64((ulong) element);
                     break;
+                
                 case ElementType.R4:
                     writer.WriteSingle((float) element);
                     break;
+                
                 case ElementType.R8:
                     writer.WriteDouble((double) element);
                     break;
+                
                 case ElementType.String:
                     writer.WriteSerString(element as string);
                     break;
+                
                 case ElementType.Object:
                     TypeSignature innerTypeSig;
-                    object value = null;
+                    object value;
 
                     if (element is null)
                     {
+                        // Most efficient way to store "null" is writing null as a string (two bytes).
                         innerTypeSig = argumentType.Module.CorLibTypeFactory.String;
+                        value = null;
                     }
                     else if (element is BoxedArgument boxedArgument)
                     {
+                        // Write the boxed argument.
                         innerTypeSig = boxedArgument.Type;
                         value = boxedArgument.Value;
                     }
@@ -108,7 +123,10 @@ namespace AsmResolver.DotNet.Signatures
                     {
                         _context.DiagnosticBag.RegisterException(new NotSupportedException(
                             $"Object elements in a custom attribute signature should be either 'null' or an instance of {nameof(BoxedArgument)}."));
-                        return;
+                        
+                        // Write null as a recovery.
+                        innerTypeSig = argumentType.Module.CorLibTypeFactory.String;
+                        value = null;
                     }
                     
                     TypeSignature.WriteFieldOrPropType(writer, innerTypeSig);
@@ -125,22 +143,55 @@ namespace AsmResolver.DotNet.Signatures
                 case ElementType.Class:
                 case ElementType.Enum:
                 case ElementType.ValueType:
-                    var enumTypeDef = argumentType.Resolve();
-                    if (enumTypeDef != null && enumTypeDef.IsEnum)
-                    {
-                        WriteElement(enumTypeDef.GetEnumUnderlyingType(), element);
-                    }
-                    else
-                    {
-                        UnsupportedArgumentType(argumentType);
-                    }
-                    
+                    WriteEnumValue(argumentType, element);
                     break;
                 
                 default:
                     UnsupportedArgumentType(argumentType);
                     break;
             }
+        }
+
+        private void WriteEnumValue(TypeSignature argumentType, object element)
+        {
+            // Try resolve enum and get enum underlying type.
+            var enumTypeDef = argumentType.Resolve();
+            if (enumTypeDef != null && enumTypeDef.IsEnum)
+            {
+                WriteElement(enumTypeDef.GetEnumUnderlyingType(), element);
+                return;
+            }
+            
+            // Enum arguments can never be null.
+            if (element is null)
+            {
+                _context.DiagnosticBag.RegisterException(
+                    new NotSupportedException($"The element of the enum-typed argument is null."));
+                
+                // Assume 0 if it is.
+                element = 0;
+            }
+
+            // Try inferring the enum type from the argument value.
+            var corLibTypeFactory = argumentType.Module.CorLibTypeFactory;
+            argumentType = Type.GetTypeCode(element.GetType()) switch
+            {
+                TypeCode.Boolean => corLibTypeFactory.Boolean,
+                TypeCode.Byte => corLibTypeFactory.Byte,
+                TypeCode.Char => corLibTypeFactory.Char,
+                TypeCode.Int16 => corLibTypeFactory.Int16,
+                TypeCode.Int32 => corLibTypeFactory.Int32,
+                TypeCode.Int64 => corLibTypeFactory.Int64,
+                TypeCode.SByte => corLibTypeFactory.SByte,
+                TypeCode.UInt16 => corLibTypeFactory.UInt16,
+                TypeCode.UInt32 => corLibTypeFactory.UInt32,
+                TypeCode.UInt64 => corLibTypeFactory.UInt64,
+                _ => _context.DiagnosticBag.RegisterExceptionAndReturnDefault<TypeSignature>(
+                    new NotSupportedException($"The element of the enum-typed argument is not of an integral type."))
+            };
+
+            if (argumentType != null)
+                WriteElement(argumentType, element);
         }
 
         private void UnsupportedArgumentType(TypeSignature argumentType)
