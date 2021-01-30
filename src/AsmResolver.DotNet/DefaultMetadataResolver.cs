8dc38aafc30116b2edba10f01ff63bc4621acdbc
Washi <washi1337@protonmail.com>
2021-01-30 21:57:35 +0100

    Replace type resolution nested type to the bottom to conform with code style.

--- src/AsmResolver.DotNet/DefaultMetadataResolver.cs
+++ src/AsmResolver.DotNet/DefaultMetadataResolver.cs
@@ -14,6 +14,128 @@ namespace AsmResolver.DotNet
         private readonly SignatureComparer _comparer = new SignatureComparer();
         private readonly IDictionary<ITypeDescriptor, TypeDefinition> _typeCache;
 
+        /// <summary>
+        /// Creates a new metadata resolver. 
+        /// </summary>
+        /// <param name="assemblyResolver">The resolver to use for resolving external assemblies.</param>
+        public DefaultMetadataResolver(IAssemblyResolver assemblyResolver)
+        {
+            AssemblyResolver = assemblyResolver ?? throw new ArgumentNullException(nameof(assemblyResolver));
+            _typeCache = new Dictionary<ITypeDescriptor, TypeDefinition>();
+        }
+
+        /// <inheritdoc />
+        public IAssemblyResolver AssemblyResolver
+        {
+            get;
+        }
+
+        /// <inheritdoc />
+        public TypeDefinition ResolveType(ITypeDescriptor type)
+        {
+            return type switch
+            {
+                TypeDefinition definition => definition,
+                TypeReference reference => ResolveTypeReference(reference),
+                TypeSpecification specification => ResolveType(specification.Signature),
+                TypeSignature signature => ResolveTypeSignature(signature),
+                ExportedType exportedType => ResolveExportedType(exportedType),
+                _ => null
+            };
+        }
+
+        private TypeDefinition LookupInCache(ITypeDescriptor type)
+        {
+            if (_typeCache.TryGetValue(type, out var typeDef))
+            {
+                // Check if type definition has changed since last lookup.
+                if (typeDef.IsTypeOf(type.Namespace, type.Name))
+                    return typeDef;
+                _typeCache.Remove(type);
+            }
+
+            return null;
+        }
+
+        private TypeDefinition ResolveTypeReference(TypeReference reference)
+        {
+            var resolvedType = LookupInCache(reference);
+            if (resolvedType != null)
+                return resolvedType;
+            
+            var resolution = new TypeResolution(AssemblyResolver);
+            resolvedType = resolution.ResolveTypeReference(reference);
+            if (resolvedType != null)
+                _typeCache[reference] = resolvedType;
+
+            return resolvedType;
+        }
+
+        private TypeDefinition ResolveExportedType(ExportedType exportedType)
+        {
+            var resolvedType = LookupInCache(exportedType);
+            if (resolvedType != null)
+                return resolvedType;
+
+            var resolution = new TypeResolution(AssemblyResolver);
+            resolvedType = resolution.ResolveExportedType(exportedType);
+            if (resolvedType != null)
+                _typeCache[exportedType] = resolvedType;
+
+            return resolvedType;
+        }
+
+        private TypeDefinition ResolveTypeSignature(TypeSignature signature)
+        {
+            var type = signature.GetUnderlyingTypeDefOrRef();
+            if (type is null)
+                return null;
+                
+            return type.MetadataToken.Table switch
+            {
+                TableIndex.TypeDef => (TypeDefinition) type,
+                TableIndex.TypeRef => ResolveTypeReference((TypeReference) type),
+                TableIndex.TypeSpec => ResolveTypeSignature(((TypeSpecification) type).Signature),
+                _ => null
+            };
+        }
+
+        /// <inheritdoc />
+        public MethodDefinition ResolveMethod(IMethodDescriptor method)
+        {
+            var declaringType = ResolveType(method.DeclaringType);
+            if (declaringType is null)
+                return null;
+
+            for (int i = 0; i < declaringType.Methods.Count; i++)
+            {
+                var candidate = declaringType.Methods[i];
+                if (candidate.Name != method.Name)
+                    continue;
+                if (!candidate.IsHideBySig || _comparer.Equals(candidate.Signature, method.Signature))
+                    return candidate;
+            }
+
+            return null;
+        }
+
+        /// <inheritdoc />
+        public FieldDefinition ResolveField(IFieldDescriptor field)
+        {
+            var declaringType = ResolveType(field.DeclaringType);
+            if (declaringType is null)
+                return null;
+
+            for (int i = 0; i < declaringType.Fields.Count; i++)
+            {
+                var candidate = declaringType.Fields[i];
+                if (candidate.Name == field.Name && _comparer.Equals(candidate.Signature, field.Signature))
+                    return candidate;
+            }
+
+            return null;
+        }
+
         private readonly struct TypeResolution
         {
             private readonly IAssemblyResolver _assemblyResolver;
@@ -144,128 +266,6 @@ namespace AsmResolver.DotNet
 
                 return null;
             }
-        }
-
-        /// <summary>
-        /// Creates a new metadata resolver. 
-        /// </summary>
-        /// <param name="assemblyResolver">The resolver to use for resolving external assemblies.</param>
-        public DefaultMetadataResolver(IAssemblyResolver assemblyResolver)
-        {
-            AssemblyResolver = assemblyResolver ?? throw new ArgumentNullException(nameof(assemblyResolver));
-            _typeCache = new Dictionary<ITypeDescriptor, TypeDefinition>();
-        }
-
-        /// <inheritdoc />
-        public IAssemblyResolver AssemblyResolver
-        {
-            get;
-        }
-
-        /// <inheritdoc />
-        public TypeDefinition ResolveType(ITypeDescriptor type)
-        {
-            return type switch
-            {
-                TypeDefinition definition => definition,
-                TypeReference reference => ResolveTypeReference(reference),
-                TypeSpecification specification => ResolveType(specification.Signature),
-                TypeSignature signature => ResolveTypeSignature(signature),
-                ExportedType exportedType => ResolveExportedType(exportedType),
-                _ => null
-            };
-        }
-
-        private TypeDefinition LookupInCache(ITypeDescriptor type)
-        {
-            if (_typeCache.TryGetValue(type, out var typeDef))
-            {
-                // Check if type definition has changed since last lookup.
-                if (typeDef.IsTypeOf(type.Namespace, type.Name))
-                    return typeDef;
-                _typeCache.Remove(type);
-            }
-
-            return null;
-        }
-
-        private TypeDefinition ResolveTypeReference(TypeReference reference)
-        {
-            var resolvedType = LookupInCache(reference);
-            if (resolvedType != null)
-                return resolvedType;
-            
-            var resolution = new TypeResolution(AssemblyResolver);
-            resolvedType = resolution.ResolveTypeReference(reference);
-            if (resolvedType != null)
-                _typeCache[reference] = resolvedType;
-
-            return resolvedType;
-        }
-
-        private TypeDefinition ResolveExportedType(ExportedType exportedType)
-        {
-            var resolvedType = LookupInCache(exportedType);
-            if (resolvedType != null)
-                return resolvedType;
-
-            var resolution = new TypeResolution(AssemblyResolver);
-            resolvedType = resolution.ResolveExportedType(exportedType);
-            if (resolvedType != null)
-                _typeCache[exportedType] = resolvedType;
-
-            return resolvedType;
-        }
-
-        private TypeDefinition ResolveTypeSignature(TypeSignature signature)
-        {
-            var type = signature.GetUnderlyingTypeDefOrRef();
-            if (type is null)
-                return null;
-                
-            return type.MetadataToken.Table switch
-            {
-                TableIndex.TypeDef => (TypeDefinition) type,
-                TableIndex.TypeRef => ResolveTypeReference((TypeReference) type),
-                TableIndex.TypeSpec => ResolveTypeSignature(((TypeSpecification) type).Signature),
-                _ => null
-            };
-        }
-
-        /// <inheritdoc />
-        public MethodDefinition ResolveMethod(IMethodDescriptor method)
-        {
-            var declaringType = ResolveType(method.DeclaringType);
-            if (declaringType is null)
-                return null;
-
-            for (int i = 0; i < declaringType.Methods.Count; i++)
-            {
-                var candidate = declaringType.Methods[i];
-                if (candidate.Name != method.Name)
-                    continue;
-                if (!candidate.IsHideBySig || _comparer.Equals(candidate.Signature, method.Signature))
-                    return candidate;
-            }
-
-            return null;
-        }
-
-        /// <inheritdoc />
-        public FieldDefinition ResolveField(IFieldDescriptor field)
-        {
-            var declaringType = ResolveType(field.DeclaringType);
-            if (declaringType is null)
-                return null;
-
-            for (int i = 0; i < declaringType.Fields.Count; i++)
-            {
-                var candidate = declaringType.Fields[i];
-                if (candidate.Name == field.Name && _comparer.Equals(candidate.Signature, field.Signature))
-                    return candidate;
-            }
-
-            return null;
         }
     }
 }
