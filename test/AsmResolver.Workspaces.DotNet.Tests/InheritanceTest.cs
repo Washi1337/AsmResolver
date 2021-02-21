3b0b51f0e9b7bd5af6152694e1f2e952046218ab
JPaja <jovan.pavlovic99@gmail.com>
2021-02-21 15:35:34 +0100

    Add: Property unit tests

--- test/AsmResolver.Workspaces.DotNet.Tests/InheritanceTest.cs
+++ test/AsmResolver.Workspaces.DotNet.Tests/InheritanceTest.cs
@@ -136,6 +136,97 @@ namespace AsmResolver.Workspaces.DotNet.Tests
             var node = workspace.Index.GetOrCreateNode(implementationMethod);
             Assert.Contains(baseMethod, node.GetRelatedObjects(DotNetRelations.ImplementationMethod));
         }
+        
+        [Fact]
+        public void ExplicitPropertyImplementation()
+        {
+            const string name = "AsmResolver.Workspaces.DotNet.TestCases.IMyInterface.ExplicitP";
 
+            var module = _fixture.Assembly.ManifestModule;
+            var implementationProperty =
+                ((TypeDefinition) module.LookupMember(typeof(MyClass).MetadataToken))
+                .Properties.First(m => m.Name == name);
+            var baseProperty = ((TypeDefinition) module.LookupMember(typeof(IMyInterface).MetadataToken))
+                .Properties.First(m => m.Name == nameof(IMyInterface.ExplicitP));
+
+            var workspace = new DotNetWorkspace();
+            workspace.Assemblies.Add(_fixture.Assembly);
+            workspace.Analyze();
+
+            var node = workspace.Index.GetOrCreateNode(implementationProperty);
+            Assert.Contains(baseProperty, node.GetRelatedObjects(DotNetRelations.ImplementationProperty));
+        }
+
+        [Fact]
+        public void ImplicitPropertyImplementation()
+        {
+            var module = _fixture.Assembly.ManifestModule;
+            var implementationProperty =
+                ((TypeDefinition) module.LookupMember(typeof(MyClass).MetadataToken))
+                .Properties.First(m => m.Name == nameof(MyClass.ImplicitP));
+            var baseProperty = ((TypeDefinition) module.LookupMember(typeof(IMyInterface).MetadataToken))
+                .Properties.First(m => m.Name == nameof(IMyInterface.ImplicitP));
+
+            var workspace = new DotNetWorkspace();
+            workspace.Assemblies.Add(_fixture.Assembly);
+            workspace.Analyze();
+
+            var node = workspace.Index.GetOrCreateNode(implementationProperty);
+            Assert.Contains(baseProperty, node.GetRelatedObjects(DotNetRelations.ImplementationProperty));
+        }
+
+        [Fact]
+        public void ImplicitPropertyOverride()
+        {
+            var module = _fixture.Assembly.ManifestModule;
+            var overrideProperty =
+                ((TypeDefinition) module.LookupMember(typeof(MyDerivedClass).MetadataToken))
+                .Properties.First(m => m.Name == nameof(MyDerivedClass.ImplicitP));
+            var baseProperty = ((TypeDefinition) module.LookupMember(typeof(MyClass).MetadataToken))
+                .Properties.First(m => m.Name == nameof(MyClass.ImplicitP));
+
+            var workspace = new DotNetWorkspace();
+            workspace.Assemblies.Add(_fixture.Assembly);
+            workspace.Analyze();
+
+            var node = workspace.Index.GetOrCreateNode(overrideProperty);
+            Assert.Contains(baseProperty, node.GetRelatedObjects(DotNetRelations.ImplementationProperty));
+        }
+
+        [Fact]
+        public void PropertyShadowingShouldNotLinkPropertiesTogether()
+        {
+            var module = _fixture.Assembly.ManifestModule;
+            var shadowedProperty =
+                ((TypeDefinition) module.LookupMember(typeof(MyDerivedClass).MetadataToken))
+                .Properties.First(m => m.Name == nameof(MyDerivedClass.ShadowedP));
+            var baseProperty = ((TypeDefinition) module.LookupMember(typeof(MyClass).MetadataToken))
+                .Properties.First(m => m.Name == nameof(MyClass.ShadowedP));
+
+            var workspace = new DotNetWorkspace();
+            workspace.Assemblies.Add(_fixture.Assembly);
+            workspace.Analyze();
+
+            var node = workspace.Index.GetOrCreateNode(shadowedProperty);
+            Assert.DoesNotContain(baseProperty, node.GetRelatedObjects(DotNetRelations.ImplementationProperty));
+        }
+
+        [Fact]
+        public void ImplicitPropertyImplementationThatIsShadowedInDerivedType()
+        {
+            var module = _fixture.Assembly.ManifestModule;
+            var implementationProperty =
+                ((TypeDefinition) module.LookupMember(typeof(MyClass).MetadataToken))
+                .Properties.First(m => m.Name == nameof(MyClass.ShadowedP));
+            var baseProperty = ((TypeDefinition) module.LookupMember(typeof(IMyInterface).MetadataToken))
+                .Properties.First(m => m.Name == nameof(IMyInterface.ShadowedP));
+
+            var workspace = new DotNetWorkspace();
+            workspace.Assemblies.Add(_fixture.Assembly);
+            workspace.Analyze();
+
+            var node = workspace.Index.GetOrCreateNode(implementationProperty);
+            Assert.Contains(baseProperty, node.GetRelatedObjects(DotNetRelations.ImplementationProperty));
+        }
     }
 }
