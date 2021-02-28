e5aadc12959d5d9b2ab047dce88f422a73efdf7c
JPaja <jovan.pavlovic99@gmail.com>
2021-03-01 00:47:50 +0100

    Fix: Generic signature check

--- src/AsmResolver.Workspaces.DotNet/Analyzers/AnalyzerUtilities.cs
+++ src/AsmResolver.Workspaces.DotNet/Analyzers/AnalyzerUtilities.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using System.Linq;
 using AsmResolver.DotNet;
 using AsmResolver.DotNet.Signatures;
+using AsmResolver.DotNet.Signatures.Types;
 
 namespace AsmResolver.Workspaces.DotNet.Analyzers
 {
@@ -11,15 +12,32 @@ namespace AsmResolver.Workspaces.DotNet.Analyzers
         {
             var declaringType = subject.DeclaringType;
 
-            var candidates = index
+            var baseTypes = index
                 .GetOrCreateNode(declaringType) // Get indexed declaring type.
                 .GetRelatedObjects(DotNetRelations.BaseType) // Get types that this declaring type is implementing.
+                .ToArray();
+            
+            var candidates = baseTypes
                 .SelectMany(type => type.Resolve()?.Methods ?? Enumerable.Empty<MethodDefinition>()) // Get the methods.
                 .Where(method => method.Name == subject.Name) // Filter on methods with the same name.
                 .ToArray();
 
+
+            var genericContexts = new List<GenericContext>();
+            foreach (var relatedObject in baseTypes)
+            {
+                if (relatedObject is not TypeSpecification {Signature: GenericInstanceTypeSignature genericSignature})
+                    continue;
+                var context = new GenericContext(genericSignature, null);
+                genericContexts.Add(context);
+            }
+
             var comparer = new SignatureComparer();
 
+            var signature = subject.Signature;
+            foreach (var t in genericContexts)
+                signature = signature.InstantiateGenericTypes(t);
+            
             for (int i = 0; i < candidates.Length; i++)
             {
                 var candidate = candidates[i];
@@ -31,7 +49,11 @@ namespace AsmResolver.Workspaces.DotNet.Analyzers
                 if (!isImplementation && !isOverride)
                     continue;
 
-                if (comparer.Equals(candidate.Signature, subject.Signature))
+                var candidateSignature = candidate.Signature;
+                foreach (var t in genericContexts)
+                    candidateSignature = candidateSignature.InstantiateGenericTypes(t);
+
+                if (comparer.Equals(candidateSignature, signature))
                     yield return candidate;
             }
         }
