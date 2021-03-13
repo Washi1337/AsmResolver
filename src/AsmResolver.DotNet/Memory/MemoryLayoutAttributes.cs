51d0295fb9267fdef95139d558e2c23ffa1615df
Washi <washi1337@protonmail.com>
2021-03-13 19:23:55 +0100

    Add xmldoc to mem layout attributes.

--- src/AsmResolver.DotNet/Memory/MemoryLayoutAttributes.cs
+++ src/AsmResolver.DotNet/Memory/MemoryLayoutAttributes.cs
@@ -2,13 +2,30 @@ using System;
 
 namespace AsmResolver.DotNet.Memory
 {
+    /// <summary>
+    /// Defines members for all possible attributes that can be assigned to a <see cref="TypeMemoryLayout"/> instance.
+    /// </summary>
     [Flags]
     public enum MemoryLayoutAttributes
     {
+        /// <summary>
+        /// Indicates the layout was determined assuming a 32-bit environment.
+        /// </summary>
         Is32Bit = 0b0,
+
+        /// <summary>
+        /// Indicates the layout was determined assuming a 32-bit environment.
+        /// </summary>
         Is64Bit = 0b1,
+
+        /// <summary>
+        /// Used to mask out the bitness of the type layout.
+        /// </summary>
         BitnessMask = 0b1,
 
+        /// <summary>
+        /// Indicates the type layout depends on the bitness of the environment.
+        /// </summary>
         IsPlatformDependent = 0b10,
     }
 }
