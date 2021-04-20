74f20bd771099fdfb6f297d08aad6ef0b522b207
oSumAtrIX <johan.melkonyan1@web.de>
2021-04-20 20:25:43 +0200

    Fix naming and add PropertyAttributes.None

--- src/AsmResolver.PE/DotNet/Metadata/Tables/Rows/PropertyAttributes.cs
+++ src/AsmResolver.PE/DotNet/Metadata/Tables/Rows/PropertyAttributes.cs
@@ -9,13 +9,17 @@ namespace AsmResolver.PE.DotNet.Metadata.Tables.Rows
     public enum PropertyAttributes : ushort
     {
         /// <summary>
+        /// Specifies that no attributes are associated with a property.
+        /// </summary>
+        None = 0x0000,  
+        /// <summary>
         /// The property uses a special name.
         /// </summary>
         SpecialName = 0x0200,
         /// <summary>
         /// The runtime should check the name encoding.
         /// </summary>
-        RtSpecialName = 0x0400,
+        RuntimeSpecialName = 0x0400,
         /// <summary>
         /// The proeprty has got a default value.
         /// </summary>
