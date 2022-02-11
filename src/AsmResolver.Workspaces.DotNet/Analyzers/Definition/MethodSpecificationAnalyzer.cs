1e92861cffcd3a3b28a73a9809e0f681d43c375e
JPaja <jovan.pavlovic99@gmail.com>
2022-02-11 21:27:45 +0100

    Add: Method Specification-Definition relation

--- src/AsmResolver.Workspaces.DotNet/Analyzers/Definition/MethodSpecificationAnalyzer.cs
+++ src/AsmResolver.Workspaces.DotNet/Analyzers/Definition/MethodSpecificationAnalyzer.cs
@@ -25,6 +25,13 @@ namespace AsmResolver.Workspaces.DotNet.Analyzers.Definition
             {
                 context.ScheduleForAnalysis(subject.DeclaringType);
             }
+
+            if (subject.Resolve() is { } definition)
+            {
+                var specification = context.Workspace.Index.GetOrCreateNode(subject);
+                var definitionNode = context.Workspace.Index.GetOrCreateNode(definition);
+                definitionNode.ForwardRelations.Add(DotNetRelations.ReferenceSpecification, specification);
+            }
         }
     }
 }
