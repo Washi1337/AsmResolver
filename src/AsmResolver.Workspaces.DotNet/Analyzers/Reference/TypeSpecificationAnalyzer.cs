102faf9296d4a5314d4110472a0eb77aecebbd9a
JPaja <jovan.pavlovic99@gmail.com>
2022-02-19 16:20:59 +0100

    Optimize TypeSpeicifaction Resolve and move to Reference folder

--- src/AsmResolver.Workspaces.DotNet/Analyzers/Reference/TypeSpecificationAnalyzer.cs
+++ src/AsmResolver.Workspaces.DotNet/Analyzers/Reference/TypeSpecificationAnalyzer.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using AsmResolver.DotNet;
 using AsmResolver.DotNet.Signatures;
 using AsmResolver.DotNet.Signatures.Types;
@@ -9,9 +10,12 @@ namespace AsmResolver.Workspaces.DotNet.Analyzers.Definition
     /// </summary>
     public class TypeSpecificationAnalyzer : ObjectAnalyzer<TypeSpecification>
     {
+        private readonly SignatureComparer _comparer = new();
+
         /// <inheritdoc />
         protected override void Analyze(AnalysisContext context, TypeSpecification subject)
         {
+
             if (subject.Signature is not null && context.HasAnalyzers(typeof(TypeSignature)))
             {
                 context.ScheduleForAnalysis(subject.Signature);
@@ -22,12 +26,23 @@ namespace AsmResolver.Workspaces.DotNet.Analyzers.Definition
                 context.ScheduleForAnalysis(subject.DeclaringType);
             }
 
-            if (subject.Resolve() is { } definition)
-            {
-                var specification = context.Workspace.Index.GetOrCreateNode(subject);
-                var definitionNode = context.Workspace.Index.GetOrCreateNode(definition);
-                definitionNode.ForwardRelations.Add(DotNetRelations.ReferenceTypeSpecification, specification);
-            }
+            if (context.Workspace is not DotNetWorkspace workspace)
+                return;
+
+            if (subject.Scope?.GetAssembly() is not { } assembly)
+                return;
+            if (!workspace.Assemblies.Any(a => _comparer.Equals(a, assembly)))
+                return;
+
+            var definition = subject.Resolve();
+            if (definition is not { Module: { Assembly: { } } })
+                return;
+            if (!workspace.Assemblies.Contains(definition.Module.Assembly))
+                return;
+
+            var specification = context.Workspace.Index.GetOrCreateNode(subject);
+            var typeNode = context.Workspace.Index.GetOrCreateNode(definition);
+            typeNode.ForwardRelations.Add(DotNetRelations.ReferenceTypeSpecification, specification);
         }
     }
 }
