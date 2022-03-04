86ab83c5820c780a78298f48517f1e1116a4123b
Washi <washi1337@protonmail.com>
2022-03-05 00:42:01 +0100

    Add HelloWorld test app with 10k dummy methods. Split benchmarks into separate classes, each for every layer of abstraction.

--- test/AsmResolver.Benchmarks/LargeMethodBodiesBenchmark.cs
+++ test/AsmResolver.Benchmarks/LargeMethodBodiesBenchmark.cs
@@ -1,5 +1,4 @@
 using System.IO;
-using System.IO.Compression;
 using AsmResolver.DotNet;
 using BenchmarkDotNet.Attributes;
 
@@ -8,25 +7,8 @@ namespace AsmResolver.Benchmarks
     [MemoryDiagnoser]
     public class LargeMethodBodiesBenchmark
     {
-        private static readonly byte[] HelloWorldPumped;
+        private static readonly byte[] HelloWorldPumped = Utilities.DecompressDeflate(Properties.Resources.HelloWorld_Pumped);
         private readonly MemoryStream _outputStream = new();
-
-        static LargeMethodBodiesBenchmark()
-        {
-            HelloWorldPumped = DecompressDeflate(Properties.Resources.HelloWorld_Pumped);
-        }
-
-        private static byte[] DecompressDeflate(byte[] compressedData)
-        {
-            using var decompressed = new MemoryStream();
-            using (var compressed = new MemoryStream(compressedData))
-            {
-                using var deflate = new DeflateStream(compressed, CompressionMode.Decompress);
-                deflate.CopyTo(decompressed);
-            }
-
-            return decompressed.ToArray();
-        }
 
         [Benchmark]
         public void HelloWorldPumped_ModuleDefinition_Read()
