d2d3d625f0c9702ba2a610cb91e3ad15b4305ca5
JPaja <jovan.pavlovic99@gmail.com>
2022-08-07 22:22:45 +0200

    Add: GenericInstanceMethodSignatureAnalyzer

---
+++ src/AsmResolver.Workspaces.DotNet/Analyzers/Signature/GenericInstanceMethodSignatureAnalyzer.cs
@@ -0,0 +1,23 @@
+﻿using AsmResolver.DotNet.Signatures;
+using AsmResolver.DotNet.Signatures.Types;
+
+namespace AsmResolver.Workspaces.DotNet.Analyzers.Signature
+{
+    /// <summary>
+    /// Provides a default implementation for an <see cref="GenericInstanceMethodSignature"/> analyzer.
+    /// </summary>
+    public class GenericInstanceMethodSignatureAnalyzer : ObjectAnalyzer<GenericInstanceMethodSignature>
+    {
+        /// <inheritdoc />
+        protected override void Analyze(AnalysisContext context, GenericInstanceMethodSignature subject)
+        {
+            if (context.HasAnalyzers(typeof(TypeSignature)))
+            {
+                for (var i = 0; i < subject.TypeArguments.Count; i++)
+                {
+                    context.ScheduleForAnalysis(subject.TypeArguments[i]);
+                }
+            }
+        }
+    }
+}
