32d2aeeed8b65096b8f7505dd6311632167809a5
Washi <washi1337@protonmail.com>
2022-08-21 18:31:34 +0200

    Remove redundant SegmentReference.CanUpdateOffsets

--- src/AsmResolver/SegmentReference.cs
+++ src/AsmResolver/SegmentReference.cs
@@ -33,9 +33,6 @@ namespace AsmResolver
         public uint Rva => Segment?.Rva ?? 0;
 
         /// <inheritdoc />
-        public bool CanUpdateOffsets => Segment?.CanUpdateOffsets ?? false;
-
-        /// <inheritdoc />
         public bool IsBounded => true;
 
         /// <inheritdoc />
