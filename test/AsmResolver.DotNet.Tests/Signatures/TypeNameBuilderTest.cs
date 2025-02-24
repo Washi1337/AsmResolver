f78bec1d68452ebd8f8655fd0936b07bcdd0c15b
ds5678 <49847914+ds5678@users.noreply.github.com>
2025-02-24 15:02:06 -0800

    Add comment

--- test/AsmResolver.DotNet.Tests/Signatures/TypeNameBuilderTest.cs
+++ test/AsmResolver.DotNet.Tests/Signatures/TypeNameBuilderTest.cs
@@ -90,6 +90,7 @@ namespace AsmResolver.DotNet.Tests.Signatures
         [Fact]
         public void CorLibWithFullScopeExactly()
         {
+            // Order matters for Mono
             var type = _module.CorLibTypeFactory.Object;
             string name = TypeNameBuilder.GetAssemblyQualifiedName(type, false);
             Assert.Equal("System.Object, System.Private.CoreLib, Version=4.0.0.0, Culture=neutral, PublicKeyToken=7cec85d7bea7798e", name);
