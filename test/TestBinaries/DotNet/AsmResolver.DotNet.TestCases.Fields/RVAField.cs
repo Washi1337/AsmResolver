928b0a8e30d6a028b2155770627ab17e1eb9e940
Aaron Robinson <aaronrobin1234@gmail.com>
2025-05-09 17:29:42 -0500

    Fix comment typo

--- test/TestBinaries/DotNet/AsmResolver.DotNet.TestCases.Fields/RVAField.cs
+++ test/TestBinaries/DotNet/AsmResolver.DotNet.TestCases.Fields/RVAField.cs
@@ -5,7 +5,7 @@ namespace AsmResolver.DotNet.TestCases.Fields
     public class RVAField
     {
         // the RVA used to initialize the array in InitialValues.cs is placed before this one
-        // since that data is 50 bytes, if we do explicitly check the alignment requirements for this RVA, it will be misaligned by 2
+        // since that data is 50 bytes, if we don't explicitly check the alignment requirements for this RVA, it will be misaligned by 2
         public static ReadOnlySpan<long> ForRVA => new long[] { 1, 2, 3, 4 };
     }
 }
