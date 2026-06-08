Washi1337/AsmResolver

commit 8291e500a0eb4a372b747b9f91dfbdddea19782c

Use pre-defined capacity in rented memory stream writer pooling

src/AsmResolver.DotNet/Code/Cil/CilMethodBodySerializer.cs
@@ -153,7 +153,7 @@ namespace AsmResolver.DotNet.Code.Cil
             {
                 var codeReader = sourceBody.OriginalRawBody.Code.CreateReader();
 
-                using var rentedWriter = _writerPool.Rent();
+                using var rentedWriter = _writerPool.Rent((int) codeReader.Length);
                 FastCilReassembler.RewriteCode(
                     ref codeReader,
                     rentedWriter.Writer,
@@ -189,7 +189,7 @@ namespace AsmResolver.DotNet.Code.Cil
                     try
                     {
                         var reader = new BinaryStreamReader(sectionData);
-                        using var rentedWriter = _writerPool.Rent();
+                        using var rentedWriter = _writerPool.Rent(sectionData.Length);
                         FastCilReassembler.RewriteExceptionHandlerSection(
                             ref reader,
                             rentedWriter.Writer,
@@ -298,7 +298,9 @@ namespace AsmResolver.DotNet.Code.Cil
 
             var bag = context.ErrorListener;
 
-            using var rentedWriter = _writerPool.Rent();
+            var lastInstruction = body.Instructions[body.Instructions.Count - 1];
+            using var rentedWriter = _writerPool.Rent(lastInstruction.Offset + lastInstruction.Size);
+
             var assembler = new CilAssembler(
                 rentedWriter.Writer,
                 new CilOperandBuilder(context.TokenProvider, bag, body.Owner),

src/AsmResolver/IO/MemoryStreamWriterPool.cs
@@ -28,6 +28,22 @@ namespace AsmResolver.IO
             return new RentedWriter(this, writer);
         }
 
+        /// <summary>
+        /// Rents a single binary stream writer.
+        /// </summary>
+        /// <param name="minimumCapacity">The minimum capacity of the backing stream.</param>
+        /// <returns>The writer.</returns>
+        public RentedWriter Rent(int minimumCapacity)
+        {
+            if (!_writers.TryDequeue(out var writer))
+                writer = new BinaryStreamWriter(new MemoryStream());
+
+            var baseStream = (MemoryStream) writer.BaseStream;
+            baseStream.SetLength(0);
+            baseStream.Capacity = Math.Max(baseStream.Capacity, minimumCapacity);
+            return new RentedWriter(this, writer);
+        }
+
         private void Return(BinaryStreamWriter writer) => _writers.Enqueue(writer);
 
         /// <summary>

src/AsmResolver/ISegment.cs
@@ -182,7 +182,7 @@ namespace AsmResolver
         /// <returns>The resulting byte array.</returns>
         public static byte[] WriteIntoArray(this ISegment segment, MemoryStreamWriterPool pool)
         {
-            using var rentedWriter = pool.Rent();
+            using var rentedWriter = pool.Rent((int) segment.GetPhysicalSize());
             segment.Write(rentedWriter.Writer);
             return rentedWriter.GetData();
         }

commit d94435ff4b78a72429f098e79fe7c06962289e41

Handle zero instruction method bodies in CilMethodBodySerializer
This fixes a regression from https://github.com/Washi1337/AsmResolver/commit/8291e500a0eb4a372b747b9f91dfbdddea19782c

src/AsmResolver.DotNet/Code/Cil/CilMethodBodySerializer.cs
@@ -1,9 +1,10 @@
-﻿using System;
+using System;
 using System.Linq;
 using AsmResolver.DotNet.Signatures;
 using AsmResolver.IO;
 using AsmResolver.PE.DotNet.Cil;
 using AsmResolver.PE.DotNet.Metadata.Tables;
+using AsmResolver.Shims;
 
 namespace AsmResolver.DotNet.Code.Cil
 {
@@ -328,6 +329,9 @@ namespace AsmResolver.DotNet.Code.Cil
             }
 
             var bag = context.ErrorListener;
+
+            if (body.Instructions.Count == 0)
+                return ArrayShim.Empty<byte>();
 
             var lastInstruction = body.Instructions[body.Instructions.Count - 1];
             using var rentedWriter = _writerPool.Rent(lastInstruction.Offset + lastInstruction.Size);

test/AsmResolver.DotNet.Tests/Builder/CilMethodBodySerializerTest.cs
@@ -50,5 +50,27 @@ namespace AsmResolver.DotNet.Tests.Builder
 
             Assert.Equal(expectedMaxStack, newModule.ManagedEntryPointMethod!.CilMethodBody!.MaxStack);
         }
+
+        [Fact]
+        public void SerializingZeroInstructionMethodBodyDoesNotThrow()
+        {
+            var module = new ModuleDefinition("SomeModule", KnownCorLibs.SystemPrivateCoreLib_v4_0_0_0);
+
+            module.GetOrCreateModuleType().Methods.Add(new MethodDefinition(
+                "Method",
+                MethodAttributes.Public | MethodAttributes.Static,
+                MethodSignature.CreateStatic(module.CorLibTypeFactory.Void))
+            {
+                CilMethodBody = new CilMethodBody()
+            });
+
+            var factory = new DotNetDirectoryFactory
+            {
+                MethodBodySerializer = new CilMethodBodySerializer()
+            };
+            var builder = new ManagedPEImageBuilder(factory, ThrowErrorListener.Instance);
+
+            builder.CreateImage(module);
+        }
     }
 }